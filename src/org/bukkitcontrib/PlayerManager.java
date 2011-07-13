0fe52a82c806ad214bd488e1900e1634fdd3f3c3
Afforess <afforess@gmail.com>
2011-07-13 11:34:56 -0700

    Alert server console on failed authentification.

--- src/org/bukkitcontrib/PlayerManager.java
+++ src/org/bukkitcontrib/PlayerManager.java
@@ -10,9 +10,6 @@ public class PlayerManager {
     private HashMap<String, Integer> timer = new HashMap<String, Integer>();
     
     public void onPlayerJoin(Player player){
-        if (!ConfigReader.isForceClient()) {
-            return;
-        }
         timer.put(player.getName(), ConfigReader.getAuthenticateTicks());
     }
     
@@ -28,16 +25,17 @@ public class PlayerManager {
                 }
                 else {
                     timer.remove(player.getName());
-                    player.kickPlayer(ConfigReader.getKickMessage());
+                    System.out.println("[BukkitContrib] Failed to authenticate " + player.getName() + "'s client in " + ConfigReader.getAuthenticateTicks() + " server ticks.");
+                    if (ConfigReader.isForceClient()) {
+                        System.out.println("[BukkitContrib] Kicking " + player.getName() + " for not running BukkitContrib client");
+                        player.kickPlayer(ConfigReader.getKickMessage());
+                    }
                 }
             }
         }
     }
     
     public void onBukkitContribSPEnable(Player player) {
-        if (!ConfigReader.isForceClient()) {
-            return;
-        }
         timer.remove(player.getName());
     }
 
