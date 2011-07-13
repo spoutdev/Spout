5b16c970e969dd256fc633a278170a9aac414931
afforess <afforess@gmail.com>
2011-07-12 21:40:53 -0400

    Exposed Get/Set name for custom inventories Support collection constructor for custom inventories

--- src/org/bukkitcontrib/inventory/CustomInventory.java
+++ src/org/bukkitcontrib/inventory/CustomInventory.java
@@ -1,13 +1,31 @@
 package org.bukkitcontrib.inventory;
 
+import java.util.Collection;
+
 import org.bukkit.inventory.ItemStack;
 
 public class CustomInventory extends ContribCraftInventory implements ContribInventory{
-	public CustomInventory(ItemStack[] items, String name) {
-		super(new CustomMCInventory(items, name));
-	}
-	
-	public CustomInventory(int size, String name) {
-		super(new CustomMCInventory(size, name));
-	}
+    public CustomInventory(ItemStack[] items, String name) {
+        super(new CustomMCInventory(items, name));
+    }
+    
+    public CustomInventory(Collection<ItemStack> items, String name) {
+        super(new CustomMCInventory(items, name));
+    }
+    
+    public CustomInventory(int size, String name) {
+        super(new CustomMCInventory(size, name));
+    }
+    
+    public CustomMCInventory getHandle() {
+        return (CustomMCInventory)super.getHandle();
+    }
+    
+    public String getName() {
+        return this.getHandle().getName();
+    }
+    
+    public void setName(String name) {
+        this.getHandle().setName(name);
+    }
 }
--- src/org/bukkitcontrib/inventory/CustomMCInventory.java
+++ src/org/bukkitcontrib/inventory/CustomMCInventory.java
@@ -1,73 +1,91 @@
 package org.bukkitcontrib.inventory;
 
+import java.util.Collection;
+
 import net.minecraft.server.EntityHuman;
 import net.minecraft.server.IInventory;
 import net.minecraft.server.ItemStack;
 
 public class CustomMCInventory implements IInventory{
-	protected ItemStack[] items;
-	protected String name;
-	
-	public CustomMCInventory(org.bukkit.inventory.ItemStack items[], String name) {
-		this.items = new ItemStack[items.length];
-		for (int i = 0; i < items.length; i++) {
-			if (items[i] == null|| items[i].getTypeId() == 0) {
-				this.items[i] = null;
-			}
-			else {
-				this.items[i] = new ItemStack(items[i].getTypeId(), items[i].getAmount(), items[i].getDurability());
-			}
-		}
-	}
-	
-	public CustomMCInventory(int size, String name) {
-		this.items = new ItemStack[size];
-		for (int i = 0; i < size; i++) {
-			this.items[i] = null;
-		}
-	}
+    protected ItemStack[] items;
+    protected String name;
+    
+    public CustomMCInventory(org.bukkit.inventory.ItemStack items[], String name) {
+        this.items = new ItemStack[items.length];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null|| items[i].getTypeId() == 0) {
+                this.items[i] = null;
+            }
+            else {
+                ContribCraftItemStack item = ContribCraftItemStack.getContribCraftItemStack(items[i]);
+                this.items[i] = item == null ? null : item.getHandle();
+            }
+        }
+    }
+    
+    public CustomMCInventory(Collection<org.bukkit.inventory.ItemStack> items, String name) {
+        this.items = new ItemStack[items.size()];
+        int pos = 0;
+        for (org.bukkit.inventory.ItemStack item : items) {
+            if (item == null|| item.getTypeId() == 0) {
+                this.items[pos] = null;
+            }
+            else {
+                ContribCraftItemStack temp = ContribCraftItemStack.getContribCraftItemStack(item);
+                this.items[pos] = temp == null ? null : temp.getHandle();
+            }
+            pos++;
+        }
+    }
+    
+    public CustomMCInventory(int size, String name) {
+        this.items = new ItemStack[size];
+        for (int i = 0; i < size; i++) {
+            this.items[i] = null;
+        }
+    }
 
-	@Override
-	public boolean a_(EntityHuman arg0) {
-		return true;
-	}
+    @Override
+    public boolean a_(EntityHuman arg0) {
+        return true;
+    }
 
-	@Override
-	public ItemStack[] getContents() {
-		return this.items;
-	}
+    @Override
+    public ItemStack[] getContents() {
+        return this.items;
+    }
 
-	@Override
-	public ItemStack getItem(int i) {
-		return this.items[i];
-	}
+    @Override
+    public ItemStack getItem(int i) {
+        return this.items[i];
+    }
 
-	@Override
-	public int getMaxStackSize() {
-		return 64;
-	}
+    @Override
+    public int getMaxStackSize() {
+        return 64;
+    }
 
-	@Override
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
+    @Override
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	@Override
-	public int getSize() {
-		return items.length;
-	}
+    @Override
+    public int getSize() {
+        return items.length;
+    }
 
-	@Override
-	public void setItem(int i, ItemStack item) {
-		this.items[i] = item;
-	}
+    @Override
+    public void setItem(int i, ItemStack item) {
+        this.items[i] = item;
+    }
 
-	@Override
-	public ItemStack splitStack(int i, int j) {
+    @Override
+    public ItemStack splitStack(int i, int j) {
         if (this.items[i] != null) {
             ItemStack itemstack;
 
@@ -79,18 +97,18 @@ public class CustomMCInventory implements IInventory{
             else {
                 itemstack = this.items[i].a(j);
                 if (this.items[i].count == 0) {
-                	this.items[i] = null;
+                    this.items[i] = null;
                 }
 
                 return itemstack;
             }
         }
         return null;
-	}
+    }
 
-	@Override
-	public void update() {
-		
-	}
+    @Override
+    public void update() {
+        
+    }
 
 }
