447dba5ecac018dbca45014a3703446a63782a48
Afforess <afforess@gmail.com>
2011-07-15 05:16:02 -0700

    Fix packet length

--- src/org/bukkitcontrib/packet/PacketPluginReload.java
+++ src/org/bukkitcontrib/packet/PacketPluginReload.java
@@ -37,7 +37,7 @@ public class PacketPluginReload implements BukkitContribPacket{
 
     @Override
     public int getNumBytes() {
-        return 9 + worldName.length();
+        return 12 + PacketUtil.getNumBytes(worldName);
     }
 
     @Override
