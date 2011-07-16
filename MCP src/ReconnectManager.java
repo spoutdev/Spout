b990b9e252103132cf37dfca76c3f31ef84e44b7
afforess <afforess@gmail.com>
2011-07-16 12:57:01 -0400

    Formatting

--- MCP src/ReconnectManager.java
+++ MCP src/ReconnectManager.java
@@ -9,42 +9,42 @@ public class ReconnectManager {
   static int portNum = -1;
 
   static public void detectKick(String infoString, String reason,
-      Object[] objects) {
-    portNum = -1;
+		Object[] objects) {
+	 portNum = -1;
 
-    if (infoString == null || reason == null || objects == null) {
-      return;
-    }
+	 if (infoString == null || reason == null || objects == null) {
+		return;
+	 }
 
-    if (objects.length == 0 || !(objects[0] instanceof String))
-      return;
+	 if (objects.length == 0 || !(objects[0] instanceof String))
+		return;
 
-    reason = (String) objects[0];
+	 reason = (String) objects[0];
 
-    if (infoString.contains("disconnect.disconnected")) {
-      if (reason.indexOf("[Serverport]") == 0
-          || reason.indexOf("[Redirect]") == 0) {
-        String[] split = reason.split(":");
-        if (split.length == 3) {
-          hostName = split[1].trim();
-          try {
-            portNum = Integer.parseInt(split[2].trim());
-          } catch (Exception e) {
-            portNum = -1;
-          }
-        } else if (split.length == 2) {
-          hostName = split[1].trim();
-          portNum = 25565;
-        }
-      }
-    }
+	 if (infoString.contains("disconnect.disconnected")) {
+		if (reason.indexOf("[Serverport]") == 0
+			 || reason.indexOf("[Redirect]") == 0) {
+		  String[] split = reason.split(":");
+		  if (split.length == 3) {
+			 hostName = split[1].trim();
+			 try {
+				portNum = Integer.parseInt(split[2].trim());
+			 } catch (Exception e) {
+				portNum = -1;
+			 }
+		  } else if (split.length == 2) {
+			 hostName = split[1].trim();
+			 portNum = 25565;
+		  }
+		}
+	 }
 
   }
 
   static public void teleport(net.minecraft.client.Minecraft mc) {
-    if (portNum != -1 && hostName != null) {
-      mc.displayGuiScreen(new GuiConnecting(mc, hostName, portNum));
-    }
+	 if (portNum != -1 && hostName != null) {
+		mc.displayGuiScreen(new GuiConnecting(mc, hostName, portNum));
+	 }
   }
 
 }
