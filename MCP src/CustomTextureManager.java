20bf8fdf8458f1abf58b72d213be8f1efcab20fe
Afforess <afforess@gmail.com>
2011-07-16 18:08:50 -0700

    Case matters

--- MCP src/CustomTextureManager.java
+++ MCP src/CustomTextureManager.java
@@ -7,26 +7,26 @@ import org.bukkitcontrib.io.FileUtil;
 import org.bukkitcontrib.io.Download;
 
 public class CustomTextureManager {
-	public static void downloadTexture(String Url) {
+	public static void downloadTexture(String url) {
 		if (!isTextureDownloading(url) && !isTextureDownloaded(url)) {
 			Download download = new Download(FileUtil.getFileName(url), FileUtil.getTextureCacheDirectory(), url, null);
 			FileDownloadThread.getInstance().addToDownloadQueue(download);
 		}
 	}
 	
-	public static boolean isTextureDownloading(String Url) {
+	public static boolean isTextureDownloading(String url) {
 		return FileDownloadThread.getInstance().isDownloading(url);
 			}
 	
-	public static boolean isTextureDownloaded(String Url) {
-		return (new File(FileUtil.getTextureCacheDirectory(), FileUtil.getFileName(Url))).exists();
+	public static boolean isTextureDownloaded(String url) {
+		return (new File(FileUtil.getTextureCacheDirectory(), FileUtil.getFileName(url))).exists();
 		}
 	
-	public static String getTextureFromUrl(String Url) {
-		if (!isTextureDownloaded(Url)) {
+	public static String getTextureFromUrl(String ul) {
+		if (!isTextureDownloaded(url)) {
 			return null;
 		}
-		File download = new File(FileUtil.getTextureCacheDirectory(), FileUtil.getFileName(Url));
+		File download = new File(FileUtil.getTextureCacheDirectory(), FileUtil.getFileName(url));
 		try {
 			return download.getCanonicalPath();
 		}
