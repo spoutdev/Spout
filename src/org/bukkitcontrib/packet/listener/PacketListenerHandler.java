51b8743d74b13a480dedd99d34460332e8b76e2f
desmin88 <billyryan24_7@yahoo.com>
2011-07-19 13:32:49 -0400

    Basic outgoing packet listeners

--- src/org/bukkitcontrib/packet/listener/PacketListenerHandler.java
+++ src/org/bukkitcontrib/packet/listener/PacketListenerHandler.java
@@ -1,32 +1,71 @@
 package org.bukkitcontrib.packet.listener;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import net.minecraft.server.Packet;
 
 public class PacketListenerHandler {
     // Hashmap for Integer (Packet ID) and ArrayList of PacketListeners allowed to listen to that packet 
-    private static final Map<Integer, PacketListener> listenerMap = new HashMap<Integer, PacketListener>();
+    private static final Map<Integer, ArrayList<PacketListener>> listenerMap = new HashMap<Integer, ArrayList<PacketListener>>();
 	
     
     
     //Method to add a listener, with PacketListener pl and the packetId it can listen on
     public static void addListener(int packetId, PacketListener pl) {
-        
+        //No other listeners are registered to this packet type, so we make a new arraylist for it.
+    	if(!listenerMap.containsKey(packetId)) {
+        	listenerMap.put(packetId, new ArrayList<PacketListener>());
+        }
+        //Now we add the packetlistener to the packetid its registered to.
+    	listenerMap.get(packetId).add(pl);
     }
     
     
     
     //Method to remove a listener, with PacketListener pl and the packetId it can listen on, because one packet listener could listen on multiple packets, in theory.
-    public static void removeListener(int packetId, PacketListener pl) {
-        
+    public static boolean removeListener(int packetId, PacketListener pl) {
+		//Checks if the anything is registered on that packetid
+    	if (!listenerMap.containsKey(packetId)) {
+			return false;
+		}
+		//Removes packet listener, checks if that packetid registry is empty, then removes it if it is empty so we dont conflict in addlistener
+    	if (listenerMap.get(packetId).remove(pl)) {
+			if (listenerMap.get(packetId).isEmpty()) {
+				listenerMap.remove(packetId);
+			}
+			return true;
+		}
+		return false;
     }
 
+    //Checks if there are any listeners at all.
+    public static boolean hasListeners() {
+    	return !listenerMap.isEmpty();
+    }
     
-    //Method to check if a packetlistner pl can listen to packet
+    //Checks if there are any listeners registered to that packetid
+	public static boolean hasListeners(int packetId) {
+		return listenerMap.containsKey(packetId);
+	}
+    
+	
+	public static List<PacketListener> getListeners(int packetId) {
+		if (hasListeners(packetId))
+			return listenerMap.get(packetId);
+		return Collections.emptyList();
+	}
+	
+	//Method to check if a packetlistner pl can listen to packet
     public static boolean checkPacket(Packet packet) {
-    	return false;
+		for (PacketListener listener : getListeners(packet.b())) {
+			if (!listener.outGoingPacket(packet, packet.b()))
+				return false;
+		}
+		return true;
     }
 
  }
