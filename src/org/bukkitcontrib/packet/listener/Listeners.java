6feb6044e4d47f3d6ea85d47db16a764bb8ded80
Ben L <nightgunner5@llamaslayers.net>
2011-07-21 09:44:15 -0500

    Add locking https://github.com/Afforess/BukkitContrib/pull/49#issuecomment-1623897

--- src/org/bukkitcontrib/packet/listener/Listeners.java
+++ src/org/bukkitcontrib/packet/listener/Listeners.java
@@ -1,6 +1,9 @@
 package org.bukkitcontrib.packet.listener;
 
 import java.util.Arrays;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
 
 import net.minecraft.server.Packet;
 
@@ -16,72 +19,113 @@ public class Listeners {
 	private Listeners() {}
 
 	private static Listener[][] listeners = new Listener[256][0];
+	private static final WriteLock lockWrite;
+	private static final ReadLock lockRead;
+	static {
+		ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+		lockWrite = lock.writeLock();
+		lockRead = lock.readLock();
+	}
 
 	public static boolean canSend(Packet packet) {
-		for (Listener listener : listeners[packet.b()]) {
-			if (!listener.checkPacket(packet))
-				return false;
+		lockRead.lock();
+		try {
+			for (Listener listener : listeners[packet.b()]) {
+				if (!listener.checkPacket(packet))
+					return false;
+			}
+			return true;
+		} finally {
+			lockRead.unlock();
 		}
-		return true;
 	}
 
 	public static void addListener(int packetId, Listener listener) {
-		if (packetId >= listeners.length)
+		if (packetId > 255)
 			return;
 
-		listeners[packetId] = Arrays.copyOf(listeners[packetId], listeners[packetId].length + 1);
-		listeners[packetId][listeners[packetId].length - 1] = listener;
+		lockWrite.lock();
+		try {
+			listeners[packetId] = Arrays.copyOf(listeners[packetId], listeners[packetId].length + 1);
+			listeners[packetId][listeners[packetId].length - 1] = listener;
+		} finally {
+			lockWrite.unlock();
+		}
 	}
 
 	public static boolean removeListener(int packetId, Listener listener) {
-		if (packetId >= listeners.length)
+		if (packetId > 255)
 			return false;
 
-		int index = -1;
-		for (int i = 0; i < listeners[packetId].length; i++) {
-			if (listeners[packetId][i] == listener) {
-				index = i;
-				break;
+		lockWrite.lock();
+		try {
+			int index = -1;
+			for (int i = 0; i < listeners[packetId].length; i++) {
+				if (listeners[packetId][i] == listener) {
+					index = i;
+					break;
+				}
 			}
+			if (index == -1)
+				return false;
+	
+			Listener[] oldListeners = listeners[packetId];
+			listeners[packetId] = new Listener[oldListeners.length - 1];
+			System.arraycopy(oldListeners, 0, listeners[packetId], 0, index);
+			System.arraycopy(oldListeners, index + 1, listeners[packetId], index, oldListeners.length - 1 - index);
+			return true;
+		} finally {
+			lockWrite.unlock();
 		}
-		if (index == -1)
-			return false;
-
-		Listener[] oldListeners = listeners[packetId];
-		listeners[packetId] = new Listener[oldListeners.length - 1];
-		System.arraycopy(oldListeners, 0, listeners[packetId], 0, index);
-		System.arraycopy(oldListeners, index + 1, listeners[packetId], index, oldListeners.length - 1 - index);
-
-		return true;
 	}
 
 	public static boolean hasListeners(int packetId) {
-		if (packetId >= listeners.length)
+		if (packetId > 255)
 			return false;
 
-		return listeners[packetId].length > 0;
+		lockRead.lock();
+		try {
+			return listeners[packetId].length > 0;
+		} finally {
+			lockRead.unlock();
+		}
 	}
 
 	public static boolean hasListeners() {
-		for (Listener[] packetListeners : listeners) {
-			if (packetListeners.length > 0)
-				return true;
+		lockRead.lock();
+		try {
+			for (Listener[] packetListeners : listeners) {
+				if (packetListeners.length > 0)
+					return true;
+			}
+			return false;
+		} finally {
+			lockRead.unlock();
 		}
-		return false;
 	}
 
 	public static boolean hasListener(int packetId, Listener listener) {
-		if (packetId >= listeners.length)
+		if (packetId > 255)
 			return false;
 
-		for (Listener packetListener : listeners[packetId]) {
-			if (packetListener == listener)
-				return true;
+		lockRead.lock();
+		try {
+			for (Listener packetListener : listeners[packetId]) {
+				if (packetListener == listener)
+					return true;
+			}
+			return false;
+		} finally {
+			lockRead.unlock();
 		}
-		return false;
 	}
 
 	public static void clearAllListeners() {
-		listeners = new Listener[256][0];
+		lockWrite.lock();
+		try {
+			listeners = new Listener[256][0];
+		} finally {
+			lockWrite.unlock();
+		}
 	}
 }
