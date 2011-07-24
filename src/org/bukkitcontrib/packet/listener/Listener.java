24f8b399b22b1ea87889046de7ec281f373753b4
raphfrk <raphfrk@gmail.com>
2011-07-24 21:11:19 +0100

    changes to atomic references for thread safety

--- src/org/bukkitcontrib/packet/listener/Listener.java
+++ src/org/bukkitcontrib/packet/listener/Listener.java
@@ -1,14 +1,16 @@
 package org.bukkitcontrib.packet.listener;
 
 import net.minecraft.server.Packet;
+import org.bukkit.entity.Player;
 
 /**
  * @author Nightgunner5
  */
 public interface Listener {
 	/**
+         * @param player The player the packet is sent to
 	 * @param packet The packet to check
 	 * @return false if the packet should be stopped, true otherwise.
 	 */
-	public boolean checkPacket(Packet packet);
+	public boolean checkPacket(Player player, Packet packet);
 }
