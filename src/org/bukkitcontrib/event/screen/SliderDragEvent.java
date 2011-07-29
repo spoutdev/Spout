15bec7cdddacfd9cc9a8767c072bc3e75c6d1a68
Afforess <afforess@gmail.com>
2011-07-29 15:12:08 -0300

    Add player

--- src/org/bukkitcontrib/event/screen/SliderDragEvent.java
+++ src/org/bukkitcontrib/event/screen/SliderDragEvent.java
@@ -2,13 +2,14 @@ package org.bukkitcontrib.event.screen;
 
 import org.bukkitcontrib.gui.Screen;
 import org.bukkitcontrib.gui.Slider;
+import org.bukkitcontrib.player.ContribPlayer
 
 public class SliderDragEvent extends ScreenEvent {
 	protected Slider slider;
 	protected float position;
 	protected float old;
-	public SliderDragEvent(Screen screen, Slider slider, float position) {
-		super("SliderDragEvent", screen);
+	public SliderDragEvent(ContribPlayer player, Screen screen, Slider slider, float position) {
+		super("SliderDragEvent", player, screen);
 		this.slider = slider;
 		this.position = position;
 		this.old = slider.getSliderPosition();
