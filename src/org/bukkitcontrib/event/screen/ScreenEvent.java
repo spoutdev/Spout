567f45a0ebe1b3d77aa30e5ccfc0268b26cc9138
Afforess <afforess@gmail.com>
2011-07-29 15:23:23 -0300

    Imports are statements too.

--- src/org/bukkitcontrib/event/screen/ScreenEvent.java
+++ src/org/bukkitcontrib/event/screen/ScreenEvent.java
@@ -3,7 +3,7 @@ package org.bukkitcontrib.event.screen;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkitcontrib.gui.Screen;
-import org.bukkitcontrib.player.ContribPlayer
+import org.bukkitcontrib.player.ContribPlayer;
 
 public abstract class ScreenEvent extends Event implements Cancellable{
 	protected Screen screen;
