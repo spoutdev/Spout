9421cc3b6342a57d0944089fc08e9272c0d88eb6
Majiir <henry@zephyrhosting.net>
2011-08-09 21:41:31 -0400

    Updated fields and functions to reflect new API double-chest semantics.

--- src/org/getspout/spout/inventory/SpoutDoubleChestInventory.java
+++ src/org/getspout/spout/inventory/SpoutDoubleChestInventory.java
@@ -6,22 +6,22 @@ import org.bukkit.block.Block;
 import org.getspout.spoutapi.inventory.DoubleChestInventory;
 
 public class SpoutDoubleChestInventory extends SpoutCraftInventory implements DoubleChestInventory{
-	protected Block left;
-	protected Block right;
-	public SpoutDoubleChestInventory(IInventory inventory, Block left, Block right) {
+	protected Block top;
+	protected Block bottom;
+	public SpoutDoubleChestInventory(IInventory inventory, Block top, Block bottom) {
 		super(inventory);
-		this.left = left;
-		this.right = right;
+		this.top = top;
+		this.bottom = bottom;
 	}
 
 	@Override
-	public Block getLeftSide() {
-		return left;
+	public Block getTopHalf() {
+		return top;
 	}
 
 	@Override
-	public Block getRightSide() {
-		return right;
+	public Block getBottomHalf() {
+		return bottom;
 	}
 
 }
