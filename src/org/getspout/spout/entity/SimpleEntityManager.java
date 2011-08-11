5365f6b7a572b56a3d3b5c43956ea334c3b59b76
Moritz <tux@moritzs-imac.fritz.box>
2011-08-11 21:47:40 +0200

    Only LivingEntities can get a new skin

--- src/org/getspout/spout/entity/SimpleEntityManager.java
+++ src/org/getspout/spout/entity/SimpleEntityManager.java
@@ -1,6 +1,6 @@
 package org.getspout.spout.entity;
 
-import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.getspout.spoutapi.entity.EntityManager;
 import org.getspout.spoutapi.packet.PacketEntitySkin;
 import org.getspout.spoutapi.player.SpoutPlayer;
@@ -8,13 +8,13 @@ import org.getspout.spoutapi.player.SpoutPlayer;
 public class SimpleEntityManager implements EntityManager {
 
 	@Override
-	public void setTexture(SpoutPlayer player, Entity entity, String texture) {
+	public void setTexture(SpoutPlayer player, LivingEntity entity, String texture) {
 		PacketEntitySkin packet = new PacketEntitySkin(entity, texture, true);
 		player.sendPacket(packet);
 	}
 
 	@Override
-	public void setAlternateTexture(SpoutPlayer player, Entity entity,
+	public void setAlternateTexture(SpoutPlayer player, LivingEntity entity,
 			String texture) {
 		PacketEntitySkin packet = new PacketEntitySkin(entity, texture, false);
 		player.sendPacket(packet);
