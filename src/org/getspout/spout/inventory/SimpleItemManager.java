1cd46a07865dd16b702203cdf3afbb49eadc5853
Top Cat <TopCat@thegigcast.net>
2011-08-13 22:15:28 +0100

    Update item name instead of reseting it

--- src/org/getspout/spout/inventory/SimpleItemManager.java
+++ src/org/getspout/spout/inventory/SimpleItemManager.java
@@ -285,7 +285,7 @@ public class SimpleItemManager implements ItemManager{
 		for (Player player : Bukkit.getServer().getOnlinePlayers()) {
 			if (player instanceof SpoutCraftPlayer){
 				if (((SpoutPlayer)player).isSpoutCraftEnabled()) {
-					((SpoutPlayer)player).sendPacket(new PacketItemName(item.getId(), (short) 0, "[reset]"));
+					((SpoutPlayer)player).sendPacket(new PacketItemName(item.getId(), (short) 0, name));
 				}
 			}
 		}
