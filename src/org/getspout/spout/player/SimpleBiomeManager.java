d0fa72188856b45b216492aa5d1d8f80fab60dc2
Dalton Pelc <djpelc@mtu.edu>
2011-08-14 23:26:27 -0400

    added first method to biomeManager

--- src/org/getspout/spout/player/SimpleBiomeManager.java
+++ src/org/getspout/spout/player/SimpleBiomeManager.java
@@ -1,7 +1,19 @@
 package org.getspout.spout.player;
 
+import org.bukkit.block.Biome;
+import org.getspout.spoutapi.packet.PacketBiomeWeather;
 import org.getspout.spoutapi.player.BiomeManager;
+import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class SimpleBiomeManager implements BiomeManager{
+
+	@Override
+	public void setPlayerBiomeWeather(SpoutPlayer player, Biome biome, byte weather) {
+		if(player.isSpoutCraftEnabled()) {
+			byte biomeByte = (byte) biome.ordinal();
+			player.sendPacket(new PacketBiomeWeather(biomeByte,weather));
+		}
+		
+	}
 
 }
