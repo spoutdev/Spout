87fefadb258a86c6ed9919cd4aad6222c1890d67
Top-Cat <Top_Cat@thegigcast.net>
2011-08-30 18:41:00 +0100

    Fix for setItemName() - Thanks nisovin

--- src/org/getspout/spout/inventory/SimpleItemManager.java
+++ src/org/getspout/spout/inventory/SimpleItemManager.java
@@ -311,7 +311,7 @@ public class SimpleItemManager implements ItemManager{
 		for (Player player : Bukkit.getServer().getOnlinePlayers()) {
 			if (player instanceof SpoutCraftPlayer){
 				if (((SpoutPlayer)player).isSpoutCraftEnabled()) {
-					((SpoutPlayer)player).sendPacket(new PacketItemName(item.getId(), (short) 0, name));
+					((SpoutPlayer)player).sendPacket(new PacketItemName(item.getId(), data, name));
 				}
 			}
 		}
