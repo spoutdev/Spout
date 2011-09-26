2a769448213c873d63a628b7d815b09b4596572f
narrowtux <narrow.m@gmail.com>
2011-09-26 20:57:04 +0200

    Final implementation of KeyBindings on server side

--- src/org/getspout/spout/keyboard/SimpleKeyBindingManager.java
+++ src/org/getspout/spout/keyboard/SimpleKeyBindingManager.java
@@ -1,15 +1,12 @@
 package org.getspout.spout.keyboard;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-import org.getspout.spout.Spout;
-import org.getspout.spoutapi.event.input.KeyPressedEvent;
-import org.getspout.spoutapi.event.input.KeyReleasedEvent;
-import org.getspout.spoutapi.gui.ScreenType;
+import org.getspout.spoutapi.event.input.KeyBindingEvent;
 import org.getspout.spoutapi.keyboard.BindingExecutionDelegate;
 import org.getspout.spoutapi.keyboard.KeyBinding;
 import org.getspout.spoutapi.keyboard.KeyBindingManager;
@@ -18,7 +15,7 @@ import org.getspout.spoutapi.packet.PacketKeyBinding;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class SimpleKeyBindingManager implements KeyBindingManager {
-	private List<KeyBinding> bindings = new ArrayList<KeyBinding>();
+	private HashMap<UUID, KeyBinding> bindings = new HashMap<UUID, KeyBinding>();
 	@Override
 	public void registerBinding(String id, Keyboard defaultKey, String description, BindingExecutionDelegate callback, Plugin plugin) throws Exception {
 		
@@ -28,7 +25,7 @@ public class SimpleKeyBindingManager implements KeyBindingManager {
 			throw new Exception("This binding is already registered: "+id+" for plugin ["+plugin.getDescription().getName()+"]");
 		}
 		KeyBinding binding = new KeyBinding(id, defaultKey, description, plugin, callback);
-		bindings.add(binding);
+		bindings.put(binding.getUniqueId(), binding);
 		for(Player p : Bukkit.getServer().getOnlinePlayers()){
 			if(p instanceof SpoutPlayer){
 				sendKeybinding((SpoutPlayer)p, binding);
@@ -36,24 +33,34 @@ public class SimpleKeyBindingManager implements KeyBindingManager {
 		}
 	}
 	
+	private KeyBinding searchBinding(String id, Plugin plugin) {
+		for(KeyBinding binding:bindings.values()) {
+			if(binding.getId().equals(id) && binding.getPlugin().equals(plugin))
+				return binding;
+		}
+		return null;
+	}
+
 	@Override
-	public void summonKey(String id, Plugin plugin, SpoutPlayer player, Keyboard key, ScreenType screenType, boolean pressed) {
-		KeyBinding binding = searchBinding(id, plugin);
+	public void summonKey(UUID uniqueId, SpoutPlayer player, Keyboard key, boolean pressed) {
+		KeyBinding binding = searchBinding(uniqueId);
 		if(binding == null)
 			return;
+		String id = binding.getId();
+		Plugin plugin = binding.getPlugin();
 		if(pressed)
 		{
-			KeyPressedEvent event = new KeyPressedEvent(key.getKeyCode(), player, screenType);
+			
 			try{
-				binding.getDelegate().keyPressed(event);
+				binding.getDelegate().keyPressed(new KeyBindingEvent(player, binding));
 			} catch(Exception e){
 				System.out.println("Could not execute Key Press Delegate of plugin ["+plugin.getDescription().getName()+"] for action ["+id+"]!");
 				e.printStackTrace();
 			}
 		} else {
-			KeyReleasedEvent event = new KeyReleasedEvent(key.getKeyCode(), player, screenType);
+			
 			try{
-				binding.getDelegate().keyReleased(event);
+				binding.getDelegate().keyReleased(new KeyBindingEvent(player, binding));
 			} catch(Exception e){
 				System.out.println("Could not execute Key Release Delegate of plugin ["+plugin.getDescription().getName()+"] for action ["+id+"]!");
 				e.printStackTrace();
@@ -61,24 +68,17 @@ public class SimpleKeyBindingManager implements KeyBindingManager {
 		}
 	}
 
+	private KeyBinding searchBinding(UUID uniqueId) {
+		return bindings.get(uniqueId);
+	}
+
 	private void sendKeybinding(SpoutPlayer p, KeyBinding binding) {
 		if(p.isSpoutCraftEnabled())
 			p.sendPacket(new PacketKeyBinding(binding));
 	}
 	
-	private KeyBinding searchBinding(String id, Plugin plugin)
-	{
-		for(KeyBinding binding:bindings)
-		{
-			if(binding.getId().equals(id) && binding.getPlugin().equals(plugin)){
-				return binding;
-			}
-		}
-		return null;
-	}
-	
 	public void onPlayerJoin(SpoutPlayer player){
-		for(KeyBinding binding:bindings)
+		for(KeyBinding binding:bindings.values())
 		{
 			sendKeybinding(player, binding);
 		}
