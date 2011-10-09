7ff46ef95bd8ed360e8b021c0eaa634229e1b71e
Top-Cat <webmaster@thomasc.co.uk>
2011-10-09 03:11:57 +0100

    Command to aid server auth

--- src/org/getspout/spout/command/SpoutCommand.java
+++ src/org/getspout/spout/command/SpoutCommand.java
@@ -1,8 +1,11 @@
 package org.getspout.spout.command;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.Player;
 import org.getspout.spout.Spout;
 import org.getspout.spout.player.SpoutCraftPlayer;
@@ -11,6 +14,7 @@ import org.getspout.spoutapi.player.SpoutPlayer;
 public class SpoutCommand implements CommandExecutor {
 
 	private final Spout p;
+	private String motd_temp = "";
 
 	public SpoutCommand(Spout p) {
 		this.p = p;
@@ -18,7 +22,6 @@ public class SpoutCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		
 		
 		sender.sendMessage("[Spout] Server version: " + p.getDescription().getVersion());
 		
@@ -58,6 +61,17 @@ public class SpoutCommand implements CommandExecutor {
 				}
 			}
 			return true;
+		} else if (c.equals("verify") && args.length > 1 && sender.isOp() && sender instanceof ConsoleCommandSender) {
+			sender.sendMessage("[Spout] Temporarily setting the motd to: " + args[1]);
+			sender.sendMessage("[Spout] It will return to its oringinal setting in ~5 mins");
+			motd_temp = ((CraftServer) Bukkit.getServer()).getHandle().server.p;
+			((CraftServer) Bukkit.getServer()).getHandle().server.p = args[1];
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(p, new Runnable() {
+				@Override
+				public void run() {
+					((CraftServer) Bukkit.getServer()).getHandle().server.p = motd_temp;
+				}
+			}, 20 * 60 * 5);
 		}
 		
 		return false;
