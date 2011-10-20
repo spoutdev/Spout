c4eeaef6bdc719057e5b4345b4c74aa7086e9b90
Afforess <afforess@gmail.com>
2011-10-20 13:17:57 -0300

    derp

--- src/org/getspout/spout/chunkstore/Utils.java
+++ src/org/getspout/spout/chunkstore/Utils.java
@@ -27,7 +27,7 @@ import java.util.Arrays;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
-import org.bukkit.World.Environment
+import org.bukkit.World.Environment;
 
 import org.bukkit.craftbukkit.CraftServer;
 
