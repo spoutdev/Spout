a1071c4a825b4af14a7e81fb33bd14c748f731cc
Afforess <afforess@gmail.com>
2011-10-28 11:16:23 -0400

    Split Material Manager into 2 classes. AbstractBlock Manager deals with Block properties. Material Manager deals with custom blocks.

--- src/org/getspout/spout/inventory/SimpleMaterialManager.java
+++ src/org/getspout/spout/inventory/SimpleMaterialManager.java
@@ -1,17 +1,12 @@
 package org.getspout.spout.inventory;
 
-import gnu.trove.iterator.TIntByteIterator;
-import gnu.trove.iterator.TIntIntIterator;
-import gnu.trove.iterator.TLongFloatIterator;
 import gnu.trove.iterator.TLongObjectIterator;
 import gnu.trove.list.array.TIntArrayList;
-import gnu.trove.map.hash.TIntByteHashMap;
 import gnu.trove.map.hash.TIntIntHashMap;
 import gnu.trove.map.hash.TIntObjectHashMap;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -29,7 +24,6 @@ import org.bukkit.util.BlockVector;
 import org.getspout.spout.Spout;
 import org.getspout.spout.block.SpoutCraftBlock;
 import org.getspout.spout.block.SpoutCraftChunk;
-import org.getspout.spout.block.mcblock.CustomMCBlock;
 import org.getspout.spout.player.SpoutCraftPlayer;
 import org.getspout.spoutapi.SpoutManager;
 import org.getspout.spoutapi.block.SpoutBlock;
@@ -41,47 +35,31 @@ import org.getspout.spoutapi.material.CustomBlock;
 import org.getspout.spoutapi.material.CustomItem;
 import org.getspout.spoutapi.material.Material;
 import org.getspout.spoutapi.material.MaterialData;
-import org.getspout.spoutapi.packet.PacketBlockData;
 import org.getspout.spoutapi.packet.PacketCustomBlockDesign;
 import org.getspout.spoutapi.packet.PacketCustomBlockOverride;
 import org.getspout.spoutapi.packet.PacketCustomItem;
 import org.getspout.spoutapi.packet.PacketCustomMultiBlockOverride;
-import org.getspout.spoutapi.packet.PacketItemName;
 import org.getspout.spoutapi.packet.PacketItemTexture;
 import org.getspout.spoutapi.packet.SpoutPacket;
 import org.getspout.spoutapi.player.SpoutPlayer;
 import org.getspout.spoutapi.util.UniqueItemStringMap;
-import org.getspout.spoutapi.util.map.TIntPairFloatHashMap;
 import org.getspout.spoutapi.util.map.TIntPairHashSet;
 import org.getspout.spoutapi.util.map.TIntPairObjectHashMap;
 
-public class SimpleMaterialManager implements MaterialManager {
-
+public class SimpleMaterialManager extends AbstractBlockManager implements MaterialManager {
 	private final TIntIntHashMap itemBlock = new TIntIntHashMap();
 	private final TIntIntHashMap itemMetaData = new TIntIntHashMap();
 	private final TIntObjectHashMap<String> itemPlugin = new TIntObjectHashMap<String>();
-
-	private final TIntPairFloatHashMap originalHardness = new TIntPairFloatHashMap();
-	private final TIntPairFloatHashMap originalFriction = new TIntPairFloatHashMap();
-	private final TIntByteHashMap originalOpacity = new TIntByteHashMap();
-	private final TIntIntHashMap originalLight = new TIntIntHashMap();
-
-	private final TIntPairObjectHashMap<String> customNames = new TIntPairObjectHashMap<String>(100);
 	private final TIntPairObjectHashMap<String> customTextures = new TIntPairObjectHashMap<String>(100);
 	private final TIntPairObjectHashMap<String> customTexturesPlugin = new TIntPairObjectHashMap<String>(100);
 	private final TIntPairObjectHashMap<ItemStack> customDrops = new TIntPairObjectHashMap<ItemStack>(100);
-
 	private final TIntPairObjectHashMap<BlockDesign> customBlockDesigns = new TIntPairObjectHashMap<BlockDesign>(100);
-
 	private final HashMap<World, TIntPairObjectHashMap<BlockOverrides>> queuedChunkBlockOverrides = new HashMap<World, TIntPairObjectHashMap<BlockOverrides>>(10);
-
-	private Set<org.getspout.spoutapi.material.Block> cachedBlockData = null;
 
 	public final static String blockIdString = "org.spout.customblocks.blockid";
 	public final static String metaDataString = "org.spout.customblocks.metadata";
 
 	public static void disableFlintStackMix() {
-
 		try {
 			Method a = Item.class.getDeclaredMethod("a", new Class[] { boolean.class });
 			a.setAccessible(true);
@@ -92,60 +70,23 @@ public class SimpleMaterialManager implements MaterialManager {
 			return;
 		}
 	}
-
-	@Override
-	public void setItemName(Material item, String name) {
-		customNames.put(item.getRawId(), item.getRawData(), name);
-		for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-			if (player instanceof SpoutCraftPlayer) {
-				if (((SpoutPlayer) player).isSpoutCraftEnabled()) {
-					((SpoutPlayer) player).sendPacket(new PacketItemName(item.getRawId(), (short) item.getRawData(), name));
-				}
-			}
-		}
-	}
-
-	@Override
-	public void resetName(Material item) {
-		int id = item.getRawId();
-		int data = item.getRawData();
-		if (customNames.containsKey(id, data)) {
-			customNames.remove(id, data);
-			for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-				if (player instanceof SpoutCraftPlayer) {
-					if (((SpoutPlayer) player).isSpoutCraftEnabled()) {
-						((SpoutPlayer) player).sendPacket(new PacketItemName(id, (short) data, "[reset]"));
-					}
-				}
-			}
-		}
-	}
-
+	
 	@Override
 	public void reset() {
-		customNames.clear();
 		customTextures.clear();
-		for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-			if (player instanceof SpoutCraftPlayer) {
-				if (((SpoutPlayer) player).isSpoutCraftEnabled()) {
-					((SpoutPlayer) player).sendPacket(new PacketItemName(0, (short) 0, "[resetall]"));
-				}
-			}
-		}
+		super.reset();
 	}
 
+	@Override
 	public void onPlayerJoin(SpoutPlayer player) {
 		if (((SpoutPlayer) player).isSpoutCraftEnabled()) {
-			for (TLongObjectIterator<String> it = customNames.iterator(); it.hasNext();) {
-				it.advance();
-				((SpoutPlayer) player).sendPacket(new PacketItemName(TIntPairHashSet.longToKey1(it.key()), (short) TIntPairHashSet.longToKey2(it.key()), it.value()));
-			}
 			for (TLongObjectIterator<String> it = customTextures.iterator(); it.hasNext();) {
 				it.advance();
 				String pluginName = (String) customTexturesPlugin.get(TIntPairHashSet.longToKey1(it.key()), (short) TIntPairHashSet.longToKey2(it.key()));
 				((SpoutPlayer) player).sendPacket(new PacketItemTexture(TIntPairHashSet.longToKey1(it.key()), (short) TIntPairHashSet.longToKey2(it.key()), pluginName, it.value()));
 			}
 		}
+		super.onPlayerJoin(player);
 	}
 
 	@Override
@@ -467,222 +408,6 @@ public class SimpleMaterialManager implements MaterialManager {
 		}
 	}
 
-	@Override
-	public String getStepSound(org.getspout.spoutapi.material.Block block) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setStepSound(org.getspout.spoutapi.material.Block block, String url) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void resetStepSound(org.getspout.spoutapi.material.Block block) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public float getFriction(org.getspout.spoutapi.material.Block block) {
-		int id = block.getRawId();
-		if(block instanceof CustomBlock) {
-			id = ((CustomBlock) block).getBlockId();
-		}
-		return net.minecraft.server.Block.byId[id].frictionFactor;
-	}
-
-	@Override
-	public void setFriction(org.getspout.spoutapi.material.Block block, float friction) {
-		int id = block.getRawId();
-		if(block instanceof CustomBlock) {
-			id = ((CustomBlock) block).getBlockId();
-		}
-		int data = block.getRawData();
-		if (!originalFriction.containsKey(id, data)) {
-			originalFriction.put(id, data, getFriction(block));
-		}
-		net.minecraft.server.Block.byId[id].frictionFactor = friction;
-		updateBlockAttributes(id, (short) data); // invalidate cache
-	}
-
-	@Override
-	public void resetFriction(org.getspout.spoutapi.material.Block block) {
-		int id = block.getRawId();
-		if(block instanceof CustomBlock) {
-			id = ((CustomBlock) block).getBlockId();
-		}
-		int data = block.getRawData();
-		if (originalFriction.containsKey(id, data)) {
-			setFriction(block, originalFriction.get(id, data));
-			originalFriction.remove(id, data);
-		}
-		updateBlockAttributes(id, (short) data); // invalidate cache
-	}
-
-	@Override
-	public float getHardness(org.getspout.spoutapi.material.Block block) {
-		int id = block.getRawId();
-		if(block instanceof CustomBlock) {
-			id = ((CustomBlock) block).getBlockId();
-		}
-		return net.minecraft.server.Block.byId[id].j();
-	}
-
-	@Override
-	public void setHardness(org.getspout.spoutapi.material.Block block, float hardness) {
-		int id = block.getRawId();
-		if(block instanceof CustomBlock) {
-			id = ((CustomBlock) block).getBlockId();
-		}
-		int data = block.getRawData();
-		if (!originalHardness.containsKey(id, data)) {
-			originalHardness.put(id, data, getHardness(block));
-		}
-		net.minecraft.server.Block b = net.minecraft.server.Block.byId[id];
-		if (b instanceof CustomMCBlock) {
-			((CustomMCBlock) b).setHardness(hardness);
-		}
-		updateBlockAttributes(id, (short) data); // invalidate cache
-	}
-
-	@Override
-	public void resetHardness(org.getspout.spoutapi.material.Block block) {
-		int id = block.getRawId();
-		if(block instanceof CustomBlock) {
-			id = ((CustomBlock) block).getBlockId();
-		}
-		int data = block.getRawData();
-		if (originalHardness.containsKey(id, data)) {
-			setHardness(block, originalHardness.get(id, data));
-			originalHardness.remove(id, data);
-		}
-		updateBlockAttributes(id, (short) data); // invalidate cache
-	}
-
-	@Override
-	public boolean isOpaque(org.getspout.spoutapi.material.Block block) {
-		int id = block.getRawId();
-		return net.minecraft.server.Block.o[id];
-	}
-
-	@Override
-	public void setOpaque(org.getspout.spoutapi.material.Block block, boolean opacity) {
-		int id = block.getRawId();
-		if(block instanceof CustomBlock) {
-			id = ((CustomBlock) block).getBlockId();
-		}
-		int data = block.getRawData();
-		if (!originalOpacity.containsKey(id)) {
-			originalOpacity.put(id, (byte) (isOpaque(block) ? 1 : 0));
-		}
-		net.minecraft.server.Block.o[id] = opacity;
-		updateBlockAttributes(id, (short) data); // invalidate cache
-	}
-
-	@Override
-	public void resetOpacity(org.getspout.spoutapi.material.Block block) {
-		int id = block.getRawId();
-		if(block instanceof CustomBlock) {
-			id = ((CustomBlock) block).getBlockId();
-		}
-		int data = block.getRawData();
-		if (originalOpacity.containsKey(id)) {
-			setOpaque(block, originalOpacity.get(id) != 0);
-			originalOpacity.remove(id);
-		}
-		updateBlockAttributes(id, (short) data); // invalidate cache
-	}
-
-	@Override
-	public int getLightLevel(org.getspout.spoutapi.material.Block block) {
-		int id = block.getRawId();
-		if(block instanceof CustomBlock) {
-			id = ((CustomBlock) block).getBlockId();
-		}
-		return net.minecraft.server.Block.s[id];
-	}
-
-	@Override
-	public void setLightLevel(org.getspout.spoutapi.material.Block block, int level) {
-		int id = block.getRawId();
-		if(block instanceof CustomBlock) {
-			id = ((CustomBlock) block).getBlockId();
-		}
-		int data = block.getRawData();
-		if (!originalLight.containsKey(id)) {
-			originalLight.put(id, getLightLevel(block));
-		}
-		net.minecraft.server.Block.s[id] = level;
-		updateBlockAttributes(id, (short) data); // invalidate cache
-	}
-
-	@Override
-	public void resetLightLevel(org.getspout.spoutapi.material.Block block) {
-		int id = block.getRawId();
-		if(block instanceof CustomBlock) {
-			id = ((CustomBlock) block).getBlockId();
-		}
-		int data = block.getRawData();
-		if (originalLight.containsKey(id)) {
-			setLightLevel(block, originalLight.get(id));
-			originalLight.remove(id);
-		}
-		updateBlockAttributes(id, (short) data); // invalidate cache
-	}
-
-	@Override
-	public Set<org.getspout.spoutapi.material.Block> getModifiedBlocks() {
-		// hit cache first
-		if (cachedBlockData != null) {
-			return cachedBlockData;
-		}
-		Set<org.getspout.spoutapi.material.Block> modified = new HashSet<org.getspout.spoutapi.material.Block>();
-		TLongFloatIterator i = originalFriction.iterator();
-		while (i.hasNext()) {
-			i.advance();
-			int id = TIntPairHashSet.longToKey1(i.key());
-			int data = TIntPairHashSet.longToKey2(i.key());
-
-			org.getspout.spoutapi.material.Block block = MaterialData.getBlock(id, (short) data);
-			if (block != null) {
-				modified.add(block);
-			}
-		}
-
-		i = originalHardness.iterator();
-		while (i.hasNext()) {
-			i.advance();
-			int id = TIntPairHashSet.longToKey1(i.key());
-			int data = TIntPairHashSet.longToKey2(i.key());
-			org.getspout.spoutapi.material.Block block = MaterialData.getBlock(id, (short) data);
-			if (block != null) {
-				modified.add(block);
-			}
-		}
-
-		TIntIntIterator j = originalLight.iterator();
-		while (j.hasNext()) {
-			j.advance();
-			org.getspout.spoutapi.material.Block block = MaterialData.getBlock(j.key());
-			if (block != null) {
-				modified.add(block);
-			}
-		}
-
-		TIntByteIterator k = originalOpacity.iterator();
-		while (k.hasNext()) {
-			k.advance();
-			org.getspout.spoutapi.material.Block block = MaterialData.getBlock(k.key());
-			if (block != null) {
-				modified.add(block);
-			}
-		}
-		cachedBlockData = modified; // save to cache
-		return modified;
-	}
 
 	@Override
 	public boolean isCustomBlock(Block block) {
@@ -722,19 +447,7 @@ public class SimpleMaterialManager implements MaterialManager {
 		return true;
 	}
 
-	private void updateBlockAttributes(int id, short data) {
-		org.getspout.spoutapi.material.Block block = MaterialData.getBlock(id, data);
-		if (block != null) {
-			cachedBlockData = null;
-			HashSet<org.getspout.spoutapi.material.Block> toUpdate = new HashSet<org.getspout.spoutapi.material.Block>(1);
-			toUpdate.add(block);
-			SpoutPacket updatePacket = new PacketBlockData(toUpdate);
-			for (SpoutPlayer player : SpoutManager.getOnlinePlayers()) {
-				if (player.isSpoutCraftEnabled())
-					player.sendPacket(updatePacket);
-			}
-		}
-	}
+
 
 	@Override
 	public boolean isCustomItem(ItemStack item) {
