2dfd3a556341bf36a73fb3d43bae108638ea304d
Robin Cloutman <rycochet@rycochet.com>
2011-12-08 05:23:28 +0000

    Add first files so this compiles...
    Commons.isSpoutcraft or Commons.isSpout can be used to check if
    it's server or client (should use this class by most things to make use
    of the JIT optimisation from the static final vars).

---
+++ src/main/java/org/getspout/commons/ServerOnly.java
@@ -0,0 +1,37 @@
+/*
+ * This file is part of SpoutCommons (http://wiki.getspout.org/).
+ * 
+ * Spout API is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Spout API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.getspout.commons;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Afforess
+ */
+@Documented
+@Target(value=ElementType.METHOD)
+@Retention(value=RetentionPolicy.SOURCE)
+public @interface ServerOnly {
+	
+	public String author() default "Afforess";
+	public String version() default "1.1";
+	public String shortDescription() default "Indicates that the function requires the use of the Spout server plugin to have any effect";
+
+}
---
+++ src/main/java/org/getspout/commons/gui/WidgetAnim.java
@@ -0,0 +1,87 @@
+/*
+ * This file is part of SpoutCommons (http://wiki.getspout.org/).
+ *
+ * Spout API is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Spout API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.getspout.commons.gui;
+
+import java.util.HashMap;
+
+/**
+ * Types of animation, only one animation is permitted at a time, and note that
+ * some types are limited to certain widget types...
+ */
+public enum WidgetAnim {
+
+	/**
+	 * No animation (default).
+	 */
+	NONE(0),
+	/**
+	 * Change the X by "value" pixels (any Widget).
+	 */
+	POS_X(1),
+	/**
+	 * Change the Y by "value" pixels (any Widget).
+	 */
+	POS_Y(2),
+	/**
+	 * Change the Width by "value" pixels (any Widget).
+	 */
+	WIDTH(3),
+	/**
+	 * Change the Height by "value" pixels (any Widget).
+	 */
+	HEIGHT(4),
+	/**
+	 * Change the Left offset by "value" pixels (Texture only).
+	 */
+	OFFSET_LEFT(5),
+	/**
+	 * Change the Top offset by "value" pixels (Texture only).
+	 */
+	OFFSET_TOP(6);
+	private final int id;
+
+	WidgetAnim(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+	private static final HashMap<Integer, WidgetAnim> lookupId = new HashMap<Integer, WidgetAnim>();
+
+	static {
+		for (WidgetAnim t : values()) {
+			lookupId.put(t.getId(), t);
+		}
+	}
+
+	public static WidgetAnim getAnimationFromId(int id) {
+		return lookupId.get(id);
+	}
+
+	public boolean check(Widget widget) {
+		switch (this) {
+			case OFFSET_TOP:
+			case OFFSET_LEFT:
+//				if (widget instanceof Texture) {
+//					return true;
+//				}
+				return false;
+		}
+		return true;
+	}
+}
