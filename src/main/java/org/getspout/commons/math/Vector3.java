0ed15e2d164018ccc9415f2eb77f7401abdb6bc6
RoyAwesome <madKylegar@gmail.com>
2011-12-15 03:55:53 -0800

    Some comments

--- src/main/java/org/getspout/commons/math/Vector3.java
+++ src/main/java/org/getspout/commons/math/Vector3.java
@@ -37,32 +37,71 @@ public class Vector3 {
 		this.z = z;
 	}
 	
+	/**
+	 * Adds two vectors
+	 * @param that
+	 * @return
+	 */
 	public Vector3 add(Vector3 that){
 		return Vector3.add(this, that);
 	}
-	
+	/** 
+	 * Subtracts two vectors
+	 * @param that
+	 * @return
+	 */
 	public Vector3 subtract(Vector3 that){
 		return Vector3.subtract(this, that);
 	}
+	/**
+	 * Scales by the scalar value
+	 * @param scale
+	 * @return
+	 */
 	public Vector3 scale(double scale){
 		return Vector3.scale(this, scale);
 	}
-	
+	/**
+	 * Takes the dot product of two vectors
+	 * @param that
+	 * @return
+	 */
 	public double dot(Vector3 that){
 		return Vector3.dot(this, that);
 	}
+	/**
+	 * Takes the cross product of two vectors
+	 * @param that
+	 * @return
+	 */
 	public Vector3 cross(Vector3 that){
 		return Vector3.cross(this, that);
 	}
+	/**
+	 * returns the squared length of the vector
+	 * @return
+	 */
 	public double lengthSquared(){
 		return Vector3.lengthSquared(this);
 	}
+	/**
+	 * returns the length of this vector.  Note: makes use of Math.sqrt and is not cached.
+	 * @return
+	 */
 	public double length(){
 		return Vector3.length(this);
 	}
+	/**
+	 * returns the vector with a length of 1
+	 * @return
+	 */
 	public Vector3 normalize(){
 		return Vector3.normalize(this);
 	}
+	/**
+	 * returns the vector as [x,y,z]
+	 * @return
+	 */
 	public double[] toArray(){
 		return Vector3.toArray(this);
 	}
