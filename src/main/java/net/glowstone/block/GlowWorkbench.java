6856b0aa48962f8328df35c70c82f23aa1999001
Skyler <skyler.welch91@gmail.com>
2011-12-15 19:15:17 +0000

    Made crafting table window pop up on workbench interaction

--- src/main/java/net/glowstone/block/GlowWorkbench.java
+++ src/main/java/net/glowstone/block/GlowWorkbench.java
@@ -1,15 +1,19 @@
 package net.glowstone.block;
 
-import net.glowstone.GlowChunk;
 import net.glowstone.entity.GlowPlayer;
+import net.glowstone.msg.OpenWindowMessage;
 import org.bukkit.Instrument;
 import org.bukkit.Note;
 import org.bukkit.block.BlockFace;
 
 public class GlowWorkbench  {
+    private static int ID = 1;
+    
     public static boolean interacted(GlowPlayer player, boolean rightClick, BlockFace against)
     {
-        player.playNote(player.getLocation(), Instrument.PIANO, new Note((byte) 1, Note.Tone.C, false));
+        player.getSession().send(new OpenWindowMessage(ID, (byte)1, "Crafting",(byte)9));
+        ID++;
+        //player.playNote(player.getLocation(), Instrument.PIANO, new Note((byte) 1, Note.Tone.C, false));
         return true;
     }
 }
