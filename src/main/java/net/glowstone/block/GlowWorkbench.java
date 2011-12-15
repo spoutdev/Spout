a34349a2dae00fe1782132d004eed95828c05d61
Skyler <skyler.welch91@gmail.com>
2011-12-15 21:07:49 +0000

    Added ability to open, close windows in GlowPlayer

--- src/main/java/net/glowstone/block/GlowWorkbench.java
+++ src/main/java/net/glowstone/block/GlowWorkbench.java
@@ -6,14 +6,13 @@ import net.glowstone.window.WindowID;
 import org.bukkit.block.BlockFace;
 
 public class GlowWorkbench  {
-   
     public static boolean interacted(GlowPlayer player, boolean rightClick, BlockFace against)
     {
         if(rightClick)
         {
             player.openWindow(WindowID.WORKBENCH);
         }
-        
+
         return true;
     }
 }
