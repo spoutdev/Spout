b2dc2158a369036f1242ec7ddd45aa4d4fe17c2b
afforess <afforess@gmail.com>
2011-12-16 00:57:24 -0500

    Rewrite GameMode for LGPL

--- src/main/java/org/getspout/api/GameMode.java
+++ src/main/java/org/getspout/api/GameMode.java
@@ -1,28 +1,12 @@
 /*
- * This file is part of Bukkit (http://bukkit.org/).
+ * This file is part of SpoutAPI (http://www.getspout.org/).
  * 
- * Bukkit is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Bukkit is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-/*
- * This file is part of SpoutcraftAPI (http://wiki.getspout.org/).
- * 
- * SpoutcraftAPI is free software: you can redistribute it and/or modify
+ * SpoutAPI is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * SpoutcraftAPI is distributed in the hope that it will be useful,
+ * SpoutAPI is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
@@ -32,52 +16,48 @@
  */
 package org.getspout.api;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
- * Represents the various type of game modes that {@link HumanEntity}s may have
+ * Is a representation of the different game modes that {@link Player}s may have.
+ * <br/>
+ * {@link GameMode#CREATIVE} implies that the player has no restrictions on flight, has infinite health, instant break, and can use any blocks.
+ * {@link GameMode#SURVIVAL} implies that the player is subject to standard game rules for health, movement, and inventory.
  */
 public enum GameMode {
-    /**
-     * Creative mode may fly, build instantly, become invulnerable and create free items
-     */
-    CREATIVE(1),
+	/**
+	 * Creative mode has no restrictions on flight, can build and break instantly, is invulnerable and can use any blocks.
+	 */
+	CREATIVE(1),
 
-    /**
-     * Survival mode is the "normal" gameplay type, with no special features.
-     */
-    SURVIVAL(0);
+	/**
+	 * Survival mode is subject to standard game rules for health, movement, and inventory.
+	 */
+	SURVIVAL(0);
 
-    private final int value;
-    private final static Map<Integer, GameMode> modes = new HashMap<Integer, GameMode>();
+	private final byte id;
+	private GameMode(final int value) {
+		this.id = (byte) value;
+	}
 
-    private GameMode(final int value) {
-        this.value = value;
-    }
+	/**
+	 * Gets the id for this GameMode
+	 *
+	 * @return GameMode id
+	 */
+	public int getId() {
+		return id;
+	}
 
-    /**
-     * Gets the mode value associated with this GameMode
-     *
-     * @return An integer value of this gamemode
-     */
-    public int getValue() {
-        return value;
-    }
-
-    /**
-     * Gets the GameMode represented by the specified value
-     *
-     * @param value Value to check
-     * @return Associative {@link GameMode} with the given value, or null if it doesn't exist
-     */
-    public static GameMode getByValue(final int value) {
-        return modes.get(value);
-    }
-
-    static {
-        for (GameMode mode : GameMode.values()) {
-            modes.put(mode.getValue(), mode);
-        }
-    }
+	/**
+	 * Gets the GameMode represented by the specified value
+	 *
+	 * @param value Value to check
+	 * @return Associative {@link GameMode} with the given value, or null if it doesn't exist
+	 */
+	public static GameMode getByValue(final int id) {
+		switch(id) {
+			case 0: return GameMode.CREATIVE;
+			case 1: return GameMode.CREATIVE;
+			default: return null;
+		}
+	}
 }
