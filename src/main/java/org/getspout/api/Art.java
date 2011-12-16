dfa77209a6d69fed78fba4f47d51ffb8615f2944
afforess <afforess@gmail.com>
2011-12-16 00:56:58 -0500

    Rewrite Art Enum for LGPL

--- src/main/java/org/getspout/api/Art.java
+++ src/main/java/org/getspout/api/Art.java
@@ -1,28 +1,12 @@
 /*
- * This file is part of Bukkit (http://bukkit.org/).
+ * This file is part of SpoutAPI (http://www.getspout.org/).
  * 
- * Bukkit is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Bukkit is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-/*
- * This file is part of SpoutcraftAPI (http://wiki.getspout.org/).
- * 
- * SpoutcraftAPI is free software: you can redistribute it and/or modify
+ * SpoutAPI is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * SpoutcraftAPI is distributed in the hope that it will be useful,
+ * SpoutAPI is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
@@ -30,47 +14,51 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.getspout.api;
+
+import gnu.trove.map.hash.TIntObjectHashMap;
 
 import java.util.HashMap;
 
 /**
- * Represents the art on a painting
+ * Represents the painting on a {@link Painting}.
  */
 public enum Art {
-	KEBAB(0,1,1),
-	AZTEC(1,1,1),
-	ALBAN(2,1,1),
-	AZTEC2(3,1,1),
-	BOMB(4,1,1),
-	PLANT(5,1,1),
-	WASTELAND(6,1,1),
-	POOL(7,2,1),
-	COURBET(8,2,1),
-	SEA(9,2,1),
-	SUNSET(10,2,1),
-	CREEBET(11,2,1),
-	WANDERER(12,1,2),
-	GRAHAM(13,1,2),
-	MATCH(14,4,2),
-	BUST(15,2,2),
-	STAGE(16,2,2),
-	VOID(17,2,2),
-	SKULL_AND_ROSES(18,2,2),
-	FIGHTERS(19,2,2),
-	POINTER(20,4,4),
-	PIGSCENE(21,4,4),
-	BURNINGSKULL(22,4,4),
-	SKELETON(23,4,3),
-	DONKEYKONG(24,4,3);
-	private int id, width, height;
-	private static HashMap<String,Art> names = new HashMap<String,Art>();
-	private static HashMap<Integer,Art> ids = new HashMap<Integer,Art>();
+	KEBAB(0, 1, 1),
+	AZTEC(1, 1, 1),
+	ALBAN(2, 1, 1),
+	AZTEC2(3, 1, 1),
+	BOMB(4, 1, 1),
+	PLANT(5, 1, 1),
+	WASTELAND(6, 1, 1),
+	WANDERER(7, 1, 2),
+	GRAHAM(8, 1, 2),
+	POOL(9, 2, 1),
+	COURBET(10, 2, 1),
+	SUNSET(11, 2, 1),
+	SEA(12, 2, 1),
+	CREEBET(13, 2, 1),
+	MATCH(14, 4, 2),
+	BUST(15, 2, 2),
+	STAGE(16, 2, 2),
+	VOID(17, 2, 2),
+	SKULL_AND_ROSES(18, 2, 2),
+	FIGHTERS(19, 2, 2),
+	SKELETON(20, 4, 3),
+	DONKEYKONG(21, 4, 3),
+	POINTER(22, 4, 4),
+	PIGSCENE(23,4, 4),
+	BURNINGSKULL(24, 4, 4),
+	;
+	private final int id;
+	private final int width;
+	private final int height;
+	private static HashMap<String, Art> names = new HashMap<String, Art>();
+	private static TIntObjectHashMap<Art> ids = new TIntObjectHashMap<Art>();
 	static {
 		for (Art art : Art.values()) {
 			ids.put(art.id, art);
-			names.put(art.toString(), art);
+			names.put(art.name(), art);
 		}
 	}
 
@@ -81,47 +69,54 @@ public enum Art {
 	}
 
 	/**
-	 * Gets the width of the painting, in blocks
+	 * Gets the width of the painting
 	 *
-	 * @return The width of the painting, in blocks
+	 * @return width
 	 */
-	public int getBlockWidth() {
+	public int getWidth() {
 		return width;
 	}
 
 	/**
-	 * Gets the height of the painting, in blocks
+	 * Gets the height of the painting
 	 *
-	 * @return The height of the painting, in blocks
+	 * @return height
 	 */
-	public int getBlockHeight() {
+	public int getHeight() {
 		return height;
 	}
 
 	/**
-	 * Get the ID of this painting.
+	 * Gets the painting id
 	 *
-	 * @return The ID of this painting
+	 * @return painting id
 	 */
 	public int getId() {
 		return id;
 	}
+	
+	@Override
+	public String toString() {
+		String name = name();
+		String start = "" + name.charAt(0);
+		return start.toUpperCase() + name.substring(1);
+	}
 
 	/**
-	 * Get a painting by its numeric ID
+	 * Search for a painting by it's id.
 	 *
-	 * @param id The ID
-	 * @return The painting
+	 * @param id painting id
+	 * @return painting if found, else null
 	 */
 	public static Art getById(int id) {
 		return ids.get(id);
 	}
 
 	/**
-	 * Get a painting by its unique name
+	 * Searches for a painting based on it's enum {@link #name()}.
 	 *
-	 * @param name The name
-	 * @return The painting
+	 * @param name to search for
+	 * @return painting if found, else null
 	 */
 	public static Art getByName(String name) {
 		return names.get(name);
