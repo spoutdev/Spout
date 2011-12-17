8fd1d45530e9749b6ccb73fb303e129b37281484
Olloth <olloth@gmail.com>
2011-12-16 19:18:12 -0500

    Wrote biome file from scratch

--- src/main/java/org/getspout/api/block/Biome.java
+++ src/main/java/org/getspout/api/block/Biome.java
@@ -1,20 +1,4 @@
 /*
- * This file is part of Bukkit (http://bukkit.org/).
- * 
- * Bukkit is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Bukkit is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-/*
  * This file is part of SpoutAPI (http://www.getspout.org/).
  * 
  * SpoutAPI is free software: you can redistribute it and/or modify
@@ -30,23 +14,19 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.getspout.api.block;
 
 public enum Biome {
-	RAINFOREST,
-	SWAMPLAND,
-	SEASONAL_FOREST,
 	FOREST,
-	SAVANNA,
-	SHRUBLAND,
 	TAIGA,
+	SWAMPLAND,
+	EXTREME_HILLS,
 	DESERT,
 	PLAINS,
-	ICE_DESERT,
-	TUNDRA,
-	HELL,
-	SKY,
 	OCEAN,
-	RIVER,
-	MOUNTAINS,
+	TUNDRA,
+	MUSHROOM,
+	NETHER,
+	END
 }
