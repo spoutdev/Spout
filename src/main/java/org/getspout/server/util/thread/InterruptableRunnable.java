1325a660de5e395489aa17b88a0b87d2d5d28e42
Raphfrk <raphfrk@gmail.com>
2011-12-18 22:12:04 +0000

    Forward planning.  Can find how to change the filename online with Github

--- src/main/java/org/getspout/server/util/thread/InterruptableRunnable.java
+++ src/main/java/org/getspout/server/util/thread/InterruptableRunnable.java
@@ -1,6 +1,6 @@
 package org.getspout.server.util.thread;
 
-public interface InterruptableRunnable {
+public interface InterruptableRunnable implements Serializable {
 
 	public void run() throws InterruptedException;
 	
