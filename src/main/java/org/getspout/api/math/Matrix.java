f018d1959d875f805d914bf8f4e15c8572336499
RoyAwesome <madKylegar@gmail.com>
2011-12-18 21:52:34 -0800

    Finishing up some matrix related things

--- src/main/java/org/getspout/api/math/Matrix.java
+++ src/main/java/org/getspout/api/math/Matrix.java
@@ -1,9 +1,25 @@
 package org.getspout.api.math;
 
+/**
+ * Representation of a square matrix
+ * 
+ * 
+ *
+ */
 public class Matrix {
 	int dimension;
 	double[] data;
 
+	/**
+	 * Creates a new 4x4 matrix, set to the Identity Matrix
+	 */
+	public Matrix(){
+		this(4);
+	}
+	/**
+	 * Creates a new matrix with the given dimension
+	 * @param dim
+	 */
 	public Matrix(int dim) {
 		dimension = dim;
 		data = new double[dim * dim];
@@ -19,27 +35,53 @@ public class Matrix {
 		return dimension;
 	}
 
+	/**
+	 * Gets the value at the given row and colum
+	 * @param row
+	 * @param column
+	 * @return
+	 */
 	public double get(int row, int column) {
 		if ((row < 0 || row > dimension)) throw new IllegalArgumentException("Row must be between 0 and " + dimension);
 		if ((column < 0 || column > dimension)) throw new IllegalArgumentException("Column must be between 0 and " + dimension);
 		return data[index(row, column, dimension)];
 	}
 
+	/**
+	 * Sets the value at the given row and column
+	 * @param row
+	 * @param column
+	 * @param value
+	 */
 	public void set(int row, int column, double value) {
 		if ((row < 0 || row > dimension)) throw new IllegalArgumentException("Row must be between 0 and " + dimension);
 		if ((column < 0 || column > dimension)) throw new IllegalArgumentException("Column must be between 0 and " + dimension);
 		data[index(row, column, dimension)] = value;
 	}
 
+	/**
+	 * Multiplies this matrix with the provided matrix
+	 * @param that
+	 * @return
+	 */
 	public Matrix multiply(Matrix that) {
 		return Matrix.multiply(this, that);
 	}
-
+	/**
+	 * Adds this matrix to the given matrix
+	 * @param that
+	 * @return
+	 */
 	public Matrix add(Matrix that) {
 		return Matrix.add(this, that);
 	}
 
-
+	/**
+	 * Adds two matricies together
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	public static Matrix add(Matrix a, Matrix b) {
 		if (a.dimension != b.dimension) throw new IllegalArgumentException("Matrix Dimensions must be equal");
 		Matrix res = new Matrix(a.dimension);
@@ -51,6 +93,12 @@ public class Matrix {
 		return res;
 	}
 
+	/**
+	 * Multiplies two matricies together
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	public static Matrix multiply(Matrix a, Matrix b) {
 		if (a.dimension != b.dimension) throw new IllegalArgumentException("Matrix Dimensions must be equal");
 		Matrix res = new Matrix(a.dimension);
@@ -70,11 +118,18 @@ public class Matrix {
 		return (y * dim + x);
 	}
 
-
+	/**
+	 * Creates and returns a 4x4 identity matrix
+	 * @return
+	 */
 	public static Matrix createIdentity() {
 		return new Matrix(4);
 	}
-
+	/**
+	 * Creates and returns a 4x4 matrix that represents the translation provided by the given Vector3
+	 * @param vector
+	 * @return
+	 */
 	public static Matrix translate(Vector3 vector) {
 		Matrix res = createIdentity();
 		res.set(0, 3, vector.getX());
@@ -82,7 +137,11 @@ public class Matrix {
 		res.set(2, 3, vector.getY());
 		return res;
 	}
-
+	/**
+	 * Creates and returns a 4x4 uniform scalar matrix
+	 * @param ammount
+	 * @return
+	 */
 	public static Matrix scale(double ammount) {
 		Matrix res = createIdentity();
 		res.set(0, 0, ammount);
@@ -90,12 +149,56 @@ public class Matrix {
 		res.set(2, 2, ammount);
 		return res;
 	}
-
+	/**
+	 * Creates and returns a 4x4 scalar matrix that scales each axis given by the provided Vector3
+	 * @param ammount
+	 * @return
+	 */
 	public static Matrix scale(Vector3 ammount) {
 		Matrix res = createIdentity();
 		res.set(0, 0, ammount.getX());
 		res.set(1, 1, ammount.getY());
 		res.set(2, 2, ammount.getZ());
 		return res;
+	}
+	/**
+	 * Creates and returns a 4x4 rotation matrix around the X axis
+	 * @param rot
+	 * @return
+	 */
+	public static Matrix rotateX(double rot){
+		Matrix res = createIdentity();
+		res.set(1, 1, MathHelper.cos(Math.toRadians(rot)));
+		res.set(1, 2, -MathHelper.sin(Math.toRadians(rot)));
+		res.set(2, 1, MathHelper.sin(Math.toRadians(rot)));
+		res.set(2, 2, MathHelper.cos(Math.toRadians(rot)));	
+		
+		return res;		
+	}
+	/**
+	 * Creates and returns a 4x4 rotation matrix around the Y axis
+	 * @param rot
+	 * @return
+	 */
+	public static Matrix rotateY(double rot){
+		Matrix res = createIdentity();
+		res.set(0, 0, MathHelper.cos(Math.toRadians(rot)));
+		res.set(0, 2, MathHelper.sin(Math.toRadians(rot)));
+		res.set(2, 0, -MathHelper.sin(Math.toRadians(rot)));
+		res.set(2, 2, MathHelper.cos(Math.toRadians(rot)));				
+		return res;		
+	}
+	/**
+	 * Creates and returns a 4x4 rotation matrix around the Z axis
+	 * @param rot
+	 * @return
+	 */
+	public static Matrix rotateZ(double rot){
+		Matrix res = createIdentity();
+		res.set(0, 0, MathHelper.cos(Math.toRadians(rot)));
+		res.set(0, 1, -MathHelper.sin(Math.toRadians(rot)));
+		res.set(1, 0, MathHelper.sin(Math.toRadians(rot)));
+		res.set(1, 1, MathHelper.cos(Math.toRadians(rot)));				
+		return res;		
 	}
 }
