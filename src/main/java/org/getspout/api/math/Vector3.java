c819237737125f268b7896c339528f05b8800173
RoyAwesome <madKylegar@gmail.com>
2011-12-18 22:27:31 -0800

    Javadocs like a boss.

--- src/main/java/org/getspout/api/math/Vector3.java
+++ src/main/java/org/getspout/api/math/Vector3.java
@@ -4,28 +4,71 @@ package org.getspout.api.math;
  * Represents a 3d vector.
  */
 public class Vector3 {
+	/**
+	 * Vector with all elements set to 0.  (0, 0, 0)
+	 */
 	public static Vector3 ZERO = new Vector3(0, 0, 0);
+	/**
+	 * Unit Vector in the X direction.  (1, 0, 0)
+	 */
 	public static Vector3 UNIT_X = new Vector3(1, 0, 0);
+	/**
+	 * Unit Vector facing Forward. (1, 0, 0)
+	 */
+	public static Vector3 Forward = UNIT_X;
+	/**
+	 * Unit Vector in the Y direction. (0, 1, 0)
+	 */
 	public static Vector3 UNIT_Y = new Vector3(0, 1, 0);
+	/**
+	 * Unit Vector pointing Up. (0, 1, 0)
+	 */
+	public static Vector3 Up = UNIT_Y;
+	/**
+	 * Unit Vector in the Z direction. (0, 0, 1)
+	 */
 	public static Vector3 UNIT_Z = new Vector3(0, 0, 1);
+	/**
+	 * Unit Vector pointing Right. (0, 0, 1)
+	 */
+	public static Vector3 Right = UNIT_Z;
+	/**
+	 * Unit Vector with all elements set to 1. (1, 1, 1)
+	 */
 	public static Vector3 ONE = new Vector3(1, 1, 1);
 
 	protected double x, y, z;
 
+	/**
+	 * Constructs a new Vector3 with the given x, y, z
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
 	public Vector3(double x, double y, double z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
-
+	/**
+	 * Constructs a new Vector3 with all elements set to 0
+	 */
 	public Vector3() {
 		this(0, 0, 0);
 	}
 
+	/**
+	 * Constructs a new Vector3 from the given Vector2 and z
+	 * @param vector
+	 * @param z
+	 */
 	public Vector3(Vector2 vector, double z) {
 		this(vector.getX(), vector.getY(), z);
 	}
-
+	/**
+	 * Constructs a new Vector3 from the given Vector2 and z set to 0
+	 * @param vector
+	 */
 	public Vector3(Vector2 vector) {
 		this(vector, 0);
 	}
@@ -119,43 +162,92 @@ public class Vector3 {
 	public double[] toArray() {
 		return Vector3.toArray(this);
 	}
-
+	/**
+	 * Returns a new vector that is a transformation of this vector around the given transformation
+	 * @param transformation
+	 * @return
+	 */
 	public Vector3 transform(Matrix transformation){
 		return Vector3.transform(this, transformation);
 	}
-
+	
+	
+	/**
+	 * Returns the length of the given vector
+	 * Note: Makes use of Math.sqrt and is not cached, so can be slow
+	 * @param a
+	 * @return
+	 */
 	public static double length(Vector3 a) {
 		return MathHelper.sqrt(lengthSquared(a));
 	}
-
+	/**
+	 * returns the length squared ot the given vector
+	 * @param a
+	 * @return
+	 */
 	public static double lengthSquared(Vector3 a) {
 		return Vector3.dot(a, a);
 	}
-
+	/**
+	 * Returns a new vector that is the given vector but length 1
+	 * @param a
+	 * @return
+	 */
 	public static Vector3 normalize(Vector3 a) {
 		return Vector3.scale(a, (1.f / a.length()));
 	}
-
+	/**
+	 * Creates a new vector that is A - B
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	public static Vector3 subtract(Vector3 a, Vector3 b) {
 		return new Vector3(a.getX() - b.getX(), a.getY() - b.getY(), a.getZ() - b.getZ());
 	}
-
+	/**
+	 * Creates a new Vector that is A + B
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	public static Vector3 add(Vector3 a, Vector3 b) {
 		return new Vector3(a.getX() + b.getX(), a.getY() + b.getY(), a.getZ() + b.getZ());
 	}
-
+	/**
+	 * Creates a new vector that is A multiplied by the uniform scalar B
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	public static Vector3 scale(Vector3 a, double b) {
 		return new Vector3(a.getX() * b, a.getY() * b, a.getZ() * b);
 	}
-
+	/**
+	 * Returns the dot product of A and B
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	public static double dot(Vector3 a, Vector3 b) {
 		return (a.getX() * b.getX() + a.getY() * b.getY() + a.getZ() + b.getZ());
 	}
-
+	/**
+	 * Creates a new Vector that is the A x B
+	 * The Cross Product is the vector orthoganal to both A and B
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	public static Vector3 cross(Vector3 a, Vector3 b) {
 		return new Vector3(a.getY() * b.getZ() - a.getZ() * b.getY(), a.getZ() * b.getX() - a.getX() * b.getZ(), a.getX() * b.getY() - a.getY() * b.getX());
 	}
-
+	/**
+	 * Returns a new double array that is {x, y, z}
+	 * @param a
+	 * @return
+	 */
 	public static double[] toArray(Vector3 a) {
 		return new double[]{a.getX(), a.getY(), a.getZ()};
 	}
