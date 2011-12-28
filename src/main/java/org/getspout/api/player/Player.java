771eca26feb6d984f591ad40c370c32492206a3f
Raphfrk <raphfrk@gmail.com>
2011-12-28 23:23:23 +0000

    Expanded Player object

--- src/main/java/org/getspout/api/player/Player.java
+++ src/main/java/org/getspout/api/player/Player.java
@@ -1,6 +1,8 @@
 package org.getspout.api.player;
 
 import org.getspout.api.entity.Entity;
+import org.getspout.api.geo.discrete.Transform;
+import org.getspout.api.protocol.Session;
 
 public interface Player {
 	
@@ -21,8 +23,22 @@ public interface Player {
 	/**
 	 * Gets the player's position.  For offline players, this is where they will appear when they login
 	 * 
-	 * @return
+	 * @return the player's position transform
 	 */
-	// TODO need to add position checks etc.
+	public Transform getTransform();
 
+	/**
+	 * Gets the session associated with the Player.
+	 * 
+	 * @return the session, or null if the player is offline
+	 */
+	public Session getSession();
+	
+	/**
+	 * Gets if the player is online
+	 * 
+	 * @return true if online
+	 */
+	public boolean isOnline();
+	
 }
