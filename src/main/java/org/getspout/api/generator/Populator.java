c531a223789fbf9a7c505c6e033fe5dc473b2b5b
RoyAwesome <madKylegar@gmail.com>
2011-12-28 14:33:59 -0800

    noone likes my capital leters :(

--- src/main/java/org/getspout/api/generator/Populator.java
+++ src/main/java/org/getspout/api/generator/Populator.java
@@ -6,5 +6,5 @@ import org.getspout.api.geo.World;
 import org.getspout.api.geo.cuboid.Chunk;
 
 public interface Populator {
-	public void Populate(Chunk c, World w, Random rng);
+	public void populate(Chunk c, World w, Random rng);
 }
