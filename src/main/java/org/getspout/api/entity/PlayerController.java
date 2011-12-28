425bd6ffb14425ef1d2e8e7a9fcc3de02e2417e1
RoyAwesome <madKylegar@gmail.com>
2011-12-27 23:44:18 -0800

    PlayerController now has an owning Player

--- src/main/java/org/getspout/api/entity/PlayerController.java
+++ src/main/java/org/getspout/api/entity/PlayerController.java
@@ -1,10 +1,20 @@
 package org.getspout.api.entity;
 
+import org.getspout.api.player.Player;
+
 /**
  * Represents a Controller that is controlled by a player
  * An entity is a Player if entity.GetController() instanceof PlayerController == true
  *
  */
 public abstract class PlayerController extends Controller {
-
+	Player owner;
+	
+	public PlayerController(Player owner){
+		this.owner = owner;
+	}
+	
+	public Player getPlayer(){
+		return owner;
+	}
 }
