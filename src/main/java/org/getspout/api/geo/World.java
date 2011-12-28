1ec7f192a2d8523bb870b55c8e3b41301fea0b72
Raphfrk <raphfrk@gmail.com>
2011-12-28 23:27:33 +0000

    Shouldn't that be a UUID not a String?

--- src/main/java/org/getspout/api/geo/World.java
+++ src/main/java/org/getspout/api/geo/World.java
@@ -1,5 +1,7 @@
 package org.getspout.api.geo;
 
+import java.util.UUID;
+
 import org.getspout.api.event.EventSource;
 import org.getspout.api.geo.cuboid.Block;
 import org.getspout.api.geo.cuboid.Chunk;
@@ -92,7 +94,7 @@ public interface World extends EventSource {
 	 * @return the name of the world
 	 */
 	@SnapshotRead
-	public String getUID();
+	public UUID getUID();
 	
 	/**
 	 * Gets the region at (x, y, z) to the given material type and returns the snapshot value
