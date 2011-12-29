011716b28be7abd7ab63fa26671f367d7398467e
RoyAwesome <madKylegar@gmail.com>
2011-12-28 18:20:06 -0800

    Added Model getters and setters

--- src/main/java/org/getspout/api/entity/Entity.java
+++ src/main/java/org/getspout/api/entity/Entity.java
@@ -2,6 +2,7 @@ package org.getspout.api.entity;
 
 import org.getspout.api.geo.discrete.Transform;
 import org.getspout.api.metadata.Metadatable;
+import org.getspout.api.model.Model;
 
 /**
  * API Entity.  
@@ -15,6 +16,13 @@ public interface Entity extends Metadatable {
 	public Controller getController() ;
 	public void setController(Controller controller);
 	public Transform getTransform();
+	
+	public void setModel(Model model);
+	public Model getModel();
+	
+	//Commented out because CollisionModel doesn't exist yet
+	//public void setCollision(CollisionModel model);
+	//public CollisionModel getCollision();
 	
 	public void onTick(float dt);
 	
