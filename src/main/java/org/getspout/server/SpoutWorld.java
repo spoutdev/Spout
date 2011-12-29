020a9158ae9bc8d343a2d6e0d8cbd20f03cf58c9
Raphfrk <raphfrk@gmail.com>
2011-12-29 00:09:00 +0000

    Expanded the world object

--- src/main/java/org/getspout/server/SpoutWorld.java
+++ src/main/java/org/getspout/server/SpoutWorld.java
@@ -1,5 +1,94 @@
 package org.getspout.server;
 
-public class SpoutWorld {
+import java.util.UUID;
+
+import org.getspout.api.geo.World;
+import org.getspout.api.geo.cuboid.Block;
+import org.getspout.api.geo.cuboid.Region;
+import org.getspout.api.geo.discrete.Point;
+
+public class SpoutWorld implements World {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getAge() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setTime(int time) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDayLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setDayLength(int time) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Block getBlock(int x, int y, int z) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Block getBlock(Point point) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UUID getUID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Region getRegion(int x, int y, int z) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public int hashCode() {
+		UUID uid = getUID();
+		long hash = uid.getMostSignificantBits();
+		hash += (hash << 5) + uid.getLeastSignificantBits();
+		
+		return (int)(hash ^ (hash >> 32));
+	}
+	
+	public boolean equals(Object obj) {
+		
+		if (obj == null) {
+			return false;
+		} else if (!(obj instanceof SpoutWorld)) {
+			return false;
+		} else {
+			SpoutWorld world = (SpoutWorld)obj;
+			
+			return world.getUID().equals(getUID());
+		}
+		
+	}
 
 }
