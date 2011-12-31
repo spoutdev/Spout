553a72e6ba4a1399f27c9ce98e88f08819bcf1e0
RoyAwesome <madKylegar@gmail.com>
2011-12-30 16:12:08 -0800

    Soft Collisions, Collisions that entities should resolve themselves.

--- src/main/java/org/getspout/api/collision/model/CollisionStrategy.java
+++ src/main/java/org/getspout/api/collision/model/CollisionStrategy.java
@@ -2,6 +2,12 @@ package org.getspout.api.collision.model;
 
 public enum CollisionStrategy {
 	/**
+	 * Indicates that the given volume is Soft, and that it counts as colliding with solid volumes, but doesn't hard resolve.  
+	 * 
+	 * Collision models should handle how softCollisions are handled.
+	 */
+	SOFT,
+	/**
 	 * Indicates that the marked Collision Volume is solid, and that other Solid volumes should 
 	 * resolve their collisions
 	 */
