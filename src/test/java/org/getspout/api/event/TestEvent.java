a632b3b0309f673b0908c38583425ffc494d6f40
zml2008 <zach@zachsthings.com>
2011-12-31 22:36:31 -0800

    Added tests for the events manager.

---
+++ src/test/java/org/getspout/api/event/TestEvent.java
@@ -0,0 +1,13 @@
+package org.getspout.api.event;
+
+public class TestEvent extends Event {
+	private static final HandlerList handlers = new HandlerList();
+
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
---
+++ src/test/java/org/getspout/api/event/TestListener.java
@@ -0,0 +1,14 @@
+package org.getspout.api.event;
+
+public class TestListener implements Listener {
+	private boolean hasBeenCalled = false;
+
+	@EventHandler(event = TestEvent.class, priority = Order.DEFAULT)
+	public void onTestEvent(TestEvent event) {
+		hasBeenCalled = true;
+	}
+
+	public boolean hasBeenCalled() {
+		return hasBeenCalled;
+	}
+}
