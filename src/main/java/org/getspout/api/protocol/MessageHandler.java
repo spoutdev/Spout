28b9374495a3ca0eab8ffac66596b68f1b4e1e11
Raphfrk <raphfrk@gmail.com>
2012-01-02 22:03:37 +0000

    Added bi-directional message handler support
    Signed-off-by: Raphfrk <raphfrk@gmail.com>

--- src/main/java/org/getspout/api/protocol/MessageHandler.java
+++ src/main/java/org/getspout/api/protocol/MessageHandler.java
@@ -1,8 +1,48 @@
 package org.getspout.api.protocol;
 
+import org.getspout.api.Commons;
 import org.getspout.api.player.Player;
 
 
 public abstract class MessageHandler<T extends Message> {
-	public abstract void handle(Session session, Player player, T message);
+	/**
+	 * Handles a message.  If the message is a one way method, then this method can be overriden.
+	 * 
+	 * Otherwise, it will call handleServer or handleClient as required.
+	 * 
+	 * @param session the network session
+	 * @param player the player
+	 * @param message the message that was received
+	 */
+	public void handle(Session session, Player player, T message) {
+		if (Commons.isSpout) {
+			handleServer(session, player, message);
+		} else {
+			handleClient(session, player, message);
+		}
+	}
+	
+	/**
+	 * Handles a message.  
+	 * 
+	 * If handle is not overriden, then this method is called when a packet is received from the client by the server.
+	 * 
+	 * @param session the network session
+	 * @param player the player
+	 * @param message the message that was received
+	 */
+	public void handleServer(Session session, Player player, T message) {
+	}
+	
+	/**
+	 * Handles a message.  
+	 * 
+	 * If handle is not overriden, then this method is called when a packet is received from the server by the client.
+	 * 
+	 * @param session the network session
+	 * @param player the player
+	 * @param message the message that was received
+	 */
+	public void handleClient(Session session, Player player, T message) {
+	}
 }
