ae86af447ebf10ef2c91cf312b73129db0a57b20
zml2008 <zach@zachsthings.com>
2012-01-03 22:08:46 -0800

    Update for naming improvements.

--- src/main/java/org/getspout/server/InternalEventListener.java
+++ src/main/java/org/getspout/server/InternalEventListener.java
@@ -1,3 +1,29 @@
+/*
+ * This file is part of Spout (http://www.getspout.org/).
+ *
+ * The Spout is licensed under the SpoutDev license version 1.
+ *
+ * Spout is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * In addition, 180 days after any changes are published, you can use the
+ * software, incorporating those changes, under the terms of the MIT license,
+ * as described in the SpoutDev License Version 1.
+ *
+ * SpoutAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License,
+ * the MIT license and the SpoutDev license version 1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
+ * License and see <http://getspout.org/SpoutDevLicenseV1.txt> for the full license,
+ * including the MIT license.
+ */
+
 package org.getspout.server;
 
 import org.getspout.api.Spout;
@@ -18,7 +44,7 @@ public class InternalEventListener implements Listener {
 		this.server = server;
 	}
 
-	@EventHandler(event = PlayerConnectEvent.class, priority = Order.MONITOR)
+	@EventHandler(event = PlayerConnectEvent.class, order = Order.MONITOR)
 	public void onPlayerConnect(PlayerConnectEvent event) {
 		if(event.isCancelled()) return;
 		//Create the player
@@ -31,7 +57,7 @@ public class InternalEventListener implements Listener {
 		}
 	}
 
-	@EventHandler(event = PlayerJoinEvent.class, priority = Order.LATEST)
+	@EventHandler(event = PlayerJoinEvent.class, order = Order.LATEST)
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		SpoutPlayer p = (SpoutPlayer)event.getPlayer();
 
