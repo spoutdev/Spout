3751956865f077f084f14e05f5748838ad8000ba
RoyAwesome <madKylegar@gmail.com>
2012-01-03 22:29:05 -0800

    Test Listener missed out on the raname train

--- src/test/java/org/getspout/api/event/TestListener.java
+++ src/test/java/org/getspout/api/event/TestListener.java
@@ -3,7 +3,7 @@ package org.getspout.api.event;
 public class TestListener implements Listener {
 	private boolean hasBeenCalled = false;
 
-	@EventHandler(event = TestEvent.class, priority = Order.DEFAULT)
+	@EventHandler(event = TestEvent.class, order = Order.DEFAULT)
 	public void onTestEvent(TestEvent event) {
 		hasBeenCalled = true;
 	}
