8b8c9dcd82655312056487ba78e90dcadfe8f0c8
Olloth <olloth@gmail.com>
2012-01-05 04:20:33 -0500

    Added recipe method interface

--- src/main/java/org/getspout/api/Game.java
+++ src/main/java/org/getspout/api/Game.java
@@ -37,8 +37,10 @@ import org.getspout.api.event.EventManager;
 import org.getspout.api.event.EventSource;
 import org.getspout.api.generator.WorldGenerator;
 import org.getspout.api.geo.World;
+import org.getspout.api.inventory.Recipe;
 import org.getspout.api.player.Player;
 import org.getspout.api.plugin.Platform;
+import org.getspout.api.plugin.Plugin;
 import org.getspout.api.plugin.PluginManager;
 import org.getspout.api.protocol.Session;
 import org.getspout.api.protocol.SessionRegistry;
@@ -366,4 +368,34 @@ public interface Game extends Named, EventSource {
 	 */
 	public Scheduler getScheduler();
 
+	/**
+	 * Registers a recipe to this games recipe database, then stores the recipe in the associated plugins recipe.yml.
+	 * If a recipe for that plugin of that name already exists, it will update the database and the recipe.yml
+	 * 
+	 * @param recipe to register
+	 */
+	public void addRecipe(Recipe recipe);
+	
+	/**
+	 * Gets a recipe registered to this games recipe database, based on the plugin and name of the recipe.
+	 * 
+	 * @param plugin that the recipe belongs to
+	 * @param recipe name
+	 * @return the recipe if it's found, otherwise null
+	 */
+	public Recipe getRecipe(Plugin plugin, String recipe);
+	
+	/**
+	 * Removes a recipe from the games recipes database, then returns the instance of it if you want to back it up.
+	 * 
+	 * *WARNING*
+	 * This will also remove the recipe from the plugins recipe.yml!
+	 * It returns a reference to the removed recipe if you want to back it up for safe keeping still.
+	 * *WARNING*
+	 * 
+	 * @param plugin that the recipe belongs to
+	 * @param recipe name
+	 * @return recipe that was removed
+	 */
+	public void removeRecipe(Plugin plugin, String recipe);
 }
