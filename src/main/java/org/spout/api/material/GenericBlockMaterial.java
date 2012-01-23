c8aa6c63557d238557742680fd1affe835cb3aa4
Afforess <afforess@gmail.com>
2012-01-23 03:11:17 -0500

    Java 1.5

--- src/main/java/org/spout/api/material/GenericBlockMaterial.java
+++ src/main/java/org/spout/api/material/GenericBlockMaterial.java
@@ -89,7 +89,6 @@ public class GenericBlockMaterial extends GenericItemMaterial implements BlockMa
 		return false;
 	}
 
-	@Override
 	public void onUpdate(World world, int x, int y, int z) {
 		
 	}
