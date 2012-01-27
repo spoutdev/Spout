9c9c222be968e49a12b5ab0dba298e54d71a13bb
alta189 <stephendwilliams7@gmail.com>
2012-01-26 22:14:13 -0500

    Formatting

--- src/main/java/org/spout/api/plugin/PluginLogger.java
+++ src/main/java/org/spout/api/plugin/PluginLogger.java
@@ -37,7 +37,7 @@ public class PluginLogger extends Logger {
 		super(plugin.getClass().getCanonicalName(), null);
 		setLevel(Level.ALL);
 		setParent(plugin.getGame().getLogger());
-	    tag = "[" + plugin.getDescription().getName() + "] ";
+		tag = "[" + plugin.getDescription().getName() + "] ";
 	}
 
 	@Override
