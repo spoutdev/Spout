1c6db58c16e4d3be255f34b52a1fba92410fe11c
RoyAwesome <madKylegar@gmail.com>
2012-01-29 22:01:20 -0800

    Added skybox block for static maps.

--- src/main/java/org/spout/api/basic/blocks/SpoutBlocks.java
+++ src/main/java/org/spout/api/basic/blocks/SpoutBlocks.java
@@ -28,7 +28,8 @@ package org.spout.api.basic.blocks;
 import org.spout.api.material.BlockMaterial;
 
 public final class SpoutBlocks {
-	public static final BlockMaterial air = new SpoutBlock("air", 0);
+	public static final BlockMaterial air = new SpoutBlock("air", 0).setOpaque(false);
 	public static final BlockMaterial solid = new SpoutBlock("solid", 1).setHardness(1.f);
 	public static final BlockMaterial unbreakable = new SpoutBlock("Unbreakable", 2).setHardness(100.f);
+	public static final BlockMaterial skybox = new SpoutBlock("Skybox", 3);
 }
