efcf5b1bcd8c77798c22f255c828c93e3f4f58b6
RoyAwesome <madKylegar@gmail.com>
2012-02-04 17:40:24 -0800

    Using SpoutNBT library

--- src/main/java/org/spout/server/util/ChannelBufferUtils.java
+++ src/main/java/org/spout/server/util/ChannelBufferUtils.java
@@ -34,15 +34,15 @@ import java.util.List;
 import java.util.Map;
 
 import org.spout.api.inventory.ItemStack;
-import org.spout.api.io.nbt.CompoundTag;
-import org.spout.api.io.nbt.NBTInputStream;
-import org.spout.api.io.nbt.NBTOutputStream;
-import org.spout.api.io.nbt.Tag;
 import org.spout.api.material.MaterialData;
 import org.spout.api.math.Vector2;
 import org.spout.api.math.Vector3;
 import org.spout.api.util.Color;
 import org.spout.api.util.Parameter;
+import org.spout.nbt.CompoundTag;
+import org.spout.nbt.Tag;
+import org.spout.nbt.stream.NBTInputStream;
+import org.spout.nbt.stream.NBTOutputStream;
 import org.jboss.netty.buffer.ChannelBuffer;
 
 /**
