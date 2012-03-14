6d482a561729f398cd56403c875f9327c7373629
Olloth <olloth@gmail.com>
2012-03-14 02:18:06 -0400

    Switched to keeping the boolean in the poolable object instead of a map
    Signed-off-by: Olloth <olloth@gmail.com>

--- src/main/java/org/spout/api/util/pool/ObjectPool.java
+++ src/main/java/org/spout/api/util/pool/ObjectPool.java
@@ -1,33 +1,29 @@
 package org.spout.api.util.pool;
 
 import java.util.Queue;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ConcurrentMap;
 
-public abstract class ObjectPool <T extends PoolableObject>{
-	
+public abstract class ObjectPool<T extends PoolableObject> {
+
 	Queue<T> pool = new ConcurrentLinkedQueue<T>();
-	ConcurrentMap<T, Boolean> map = new ConcurrentHashMap<T, Boolean>();
-	
-	public T getInstance(){
-		if(pool.isEmpty()) {
+
+	public T getInstance() {
+		if (pool.isEmpty()) {
 			T t = createNew();
 			t.pool(this);
 			return t;
 		}
 		T obj = pool.poll();
-		map.remove(obj);
+		obj.isFreed = false;
 		return obj;
 	}
-	
-	
+
 	protected abstract T createNew();
 
-	public void reclaim(T object){
-		if(map.containsKey(object)) throw new IllegalArgumentException("Cannot reclaim something that the pool already contains");
+	public void reclaim(T object) {
+		if (object.isFreed) {
+			throw new IllegalArgumentException("Cannot reclaim something that the pool already contains");
+		}
 		pool.add(object);
-		map.put(object, Boolean.FALSE);
 	}
-
 }
--- src/main/java/org/spout/api/util/pool/PoolableObject.java
+++ src/main/java/org/spout/api/util/pool/PoolableObject.java
@@ -1,26 +1,29 @@
 package org.spout.api.util.pool;
 
 public abstract class PoolableObject {
-	
-	
+
 	boolean isPooled;
+	boolean isFreed;
 	ObjectPool parentPool;
-	
-	
-	protected void pool(ObjectPool p){
-		if(isPooled) throw new IllegalArgumentException("Object already pooled! Cannot pool again");
+
+	protected void pool(ObjectPool p) {
+		if (isPooled) {
+			throw new IllegalArgumentException("Object already pooled! Cannot pool again");
+		}
 		isPooled = true;
+		isFreed = false;
 		parentPool = p;
 	}
-	
-	public void free(){
+
+	public void free() {
+		isFreed = true;
 		parentPool.reclaim(this);
 	}
-	
-	@Override
-	public void finalize(){
-		if(this.isPooled) free();
-		
-	}
 
+	@Override
+	public void finalize() {
+		if (isPooled) {
+			free();
+		}
+	}
 }
