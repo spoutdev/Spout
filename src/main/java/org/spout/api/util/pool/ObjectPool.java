7ce9af0dc940a994405f947269a033fbb5c213ca
RoyAwesome <madKylegar@gmail.com>
2012-03-13 23:55:56 -0700

    Check to see if pool owns the object that is being reclaimed Javadoc and headers

--- src/main/java/org/spout/api/util/pool/ObjectPool.java
+++ src/main/java/org/spout/api/util/pool/ObjectPool.java
@@ -1,3 +1,28 @@
+/*
+ * This file is part of SpoutAPI (http://www.spout.org/).
+ *
+ * SpoutAPI is licensed under the SpoutDev License Version 1.
+ *
+ * SpoutAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * In addition, 180 days after any changes are published, you can use the
+ * software, incorporating those changes, under the terms of the MIT license,
+ * as described in the SpoutDev License Version 1.
+ *
+ * SpoutAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License,
+ * the MIT license and the SpoutDev License Version 1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
+ * License and see <http://www.spout.org/SpoutDevLicenseV1.txt> for the full license,
+ * including the MIT license.
+ */
 package org.spout.api.util.pool;
 
 import java.util.Queue;
@@ -7,6 +32,12 @@ public abstract class ObjectPool<T extends PoolableObject> {
 
 	Queue<T> pool = new ConcurrentLinkedQueue<T>();
 
+	/**
+	 * Returns an object out of the pool.  
+	 * If the pool is exhausted, then a new object will be created.
+	 * 
+	 * @return
+	 */
 	public T getInstance() {
 		if (pool.isEmpty()) {
 			T t = createNew();
@@ -20,9 +51,17 @@ public abstract class ObjectPool<T extends PoolableObject> {
 
 	protected abstract T createNew();
 
+	/**
+	 * Returns an object into the pool.
+	 * 
+	 * @param object
+	 */
 	public void reclaim(T object) {
 		if (object.isFreed) {
 			throw new IllegalArgumentException("Cannot reclaim something that the pool already contains");
+		}
+		if(object.parentPool != this){
+			throw new IllegalArgumentException("Cannot reclaim something that isn't owned by this pool");
 		}
 		pool.add(object);
 	}
