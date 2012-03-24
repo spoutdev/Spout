10b45a9d0e2377c577bf39608a3f93c795032002
RoyAwesome <madKylegar@gmail.com>
2012-03-24 01:29:07 -0700

    Fixed failing test with Unit vectors

--- src/test/java/org/spout/api/math/Vector3Test.java
+++ src/test/java/org/spout/api/math/Vector3Test.java
@@ -51,11 +51,11 @@ public class Vector3Test {
 		testValue(Vector3.ONE, 1, 1, 1);
 		testValue(Vector3.ZERO, 0, 0, 0);
 		testValue(Vector3.UNIT_X, 1, 0, 0);
-		testValue(Vector3.Forward, Vector3.UNIT_X);
+		testValue(Vector3.Forward, Vector3.UNIT_Z);
 		testValue(Vector3.UNIT_Y, 0, 1, 0);
 		testValue(Vector3.Up, Vector3.UNIT_Y);
 		testValue(Vector3.UNIT_Z, 0, 0, 1);
-		testValue(Vector3.Right, Vector3.UNIT_Z);
+		testValue(Vector3.Right, Vector3.UNIT_X);
 	}
 
 	@Test
