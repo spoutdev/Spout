a6694f977d71f63e0d1d7b8facb953725d658eed
RoyAwesome <madKylegar@gmail.com>
2012-04-05 01:03:28 -0700

    Only create client related folders on the client. Added headers to some files that i forgot (many more need headers)

--- src/main/java/org/spout/engine/filesystem/FileSystem.java
+++ src/main/java/org/spout/engine/filesystem/FileSystem.java
@@ -1,6 +1,35 @@
+/*
+ * This file is part of Spout (http://www.spout.org/).
+ *
+ * Spout is licensed under the SpoutDev License Version 1.
+ *
+ * Spout is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * In addition, 180 days after any changes are published, you can use the
+ * software, incorporating those changes, under the terms of the MIT license,
+ * as described in the SpoutDev License Version 1.
+ *
+ * Spout is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License,
+ * the MIT license and the SpoutDev License Version 1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
+ * License and see <http://www.spout.org/SpoutDevLicenseV1.txt> for the full license,
+ * including the MIT license.
+ */
 package org.spout.engine.filesystem;
 
 import java.io.File;
+
+import org.spout.api.plugin.Platform;
+
+import org.spout.api.Spout;
 
 public class FileSystem {
 
@@ -18,11 +47,11 @@ public class FileSystem {
 	
 	
 	static {
-		if(!resourceFolder.exists()) resourceFolder.mkdirs();
-		if(!cacheFolder.exists()) cacheFolder.mkdirs();
+		if(Spout.getPlatform() == Platform.CLIENT && !resourceFolder.exists()) resourceFolder.mkdirs();
+		if(Spout.getPlatform() == Platform.CLIENT && !cacheFolder.exists()) cacheFolder.mkdirs();
 		if(!pluginDirectory.exists()) pluginDirectory.mkdirs();
-		if(!configDirectory.exists()) configDirectory.mkdirs();
-		if(!updateDirectory.exists()) updateDirectory.mkdirs();
+		if(Spout.getPlatform() == Platform.SERVER && !configDirectory.exists()) configDirectory.mkdirs();
+		if(Spout.getPlatform() == Platform.SERVER && !updateDirectory.exists()) updateDirectory.mkdirs();
 		if(!dataDirectory.exists()) dataDirectory.mkdirs();
 		if(!worldsDirectory.exists()) worldsDirectory.mkdirs();
 		
