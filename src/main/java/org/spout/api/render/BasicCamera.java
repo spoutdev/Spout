f2881017a66990f708cee06b5bad7398103a1ddb
RoyAwesome <madKylegar@gmail.com>
2012-04-07 15:30:25 -0700

    Added basic camera

---
+++ src/main/java/org/spout/api/render/BasicCamera.java
@@ -0,0 +1,55 @@
+/*
+ * This file is part of SpoutAPI (http://www.spout.org/).
+ *
+ * SpoutAPI is licensed under the SpoutDev License Version 1.
+ *
+ * SpoutAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * In addition, 180 days after any changes are published, you can use the
+ * software, incorporating those changes, under the terms of the MIT license,
+ * as described in the SpoutDev License Version 1.
+ *
+ * SpoutAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License,
+ * the MIT license and the SpoutDev License Version 1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
+ * License and see <http://www.spout.org/SpoutDevLicenseV1.txt> for the full license,
+ * including the MIT license.
+ */
+package org.spout.api.render;
+
+import org.spout.api.math.Matrix;
+
+public class BasicCamera implements Camera {
+	
+	private Matrix projection;
+	private Matrix view;
+	
+	public BasicCamera(Matrix proj, Matrix view){
+		this.projection = proj;
+		this.view = view;
+	}
+	
+	@Override
+	public Matrix getProjection() {
+		return projection;
+	}
+
+	@Override
+	public Matrix getView() {
+		return view;
+	}
+
+	@Override
+	public void updateView() {
+		//This function...It does nothing!
+	}
+
+}
