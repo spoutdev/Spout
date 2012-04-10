2a0cc32bfe72a62a8100c1239a510f8e26c0ca11
RoyAwesome <madKylegar@gmail.com>
2012-04-09 23:33:56 -0700

    You should be uncommented now, textures work

--- src/main/java/org/spout/api/render/Shader.java
+++ src/main/java/org/spout/api/render/Shader.java
@@ -23,7 +23,7 @@ public interface Shader {
 
 	public abstract void setUniform(String name, Color value);
 
-	//public abstract void setUniform(String name, Texture value);
+	public abstract void setUniform(String name, Texture value);
 
 	public abstract void enableAttribute(String name, int size, int type, int stride, long offset);
 
