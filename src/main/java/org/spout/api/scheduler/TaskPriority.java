270978ab41ce20556c8e2200b065cadee7d9bbcf
Raphfrk <raphfrk@gmail.com>
2012-05-20 21:50:03 +0100

    Added more default priority settings

--- src/main/java/org/spout/api/scheduler/TaskPriority.java
+++ src/main/java/org/spout/api/scheduler/TaskPriority.java
@@ -3,21 +3,33 @@ package org.spout.api.scheduler;
 public class TaskPriority {
 
 	/**
-	 * Priority for tasks which should not be deferred
+	 * Priority for tasks which may not be deferred
 	 */
 	public static final TaskPriority CRITICAL = new TaskPriority(0);
 	/**
-	 * Priority for tasks which can be deferred by up to 50ms
+	 * Priority for tasks which can be deferred by up to 50ms when under load
 	 */
-	public static final TaskPriority HIGH = new TaskPriority(50);
+	public static final TaskPriority HIGHEST = new TaskPriority(50);
 	/**
-	 * Priority for tasks which can be deferred by up to 500ms
+	 * Priority for tasks which can be deferred by up to 150ms when under load
+	 */
+	public static final TaskPriority HIGH = new TaskPriority(150);
+	/**
+	 * Priority for tasks which can be deferred by up to 500ms when under load
 	 */
 	public static final TaskPriority MEDIUM = new TaskPriority(500);
 	/**
-	 * Priority for tasks which can be deferred by up to 10s
+	 * Priority for tasks which can be deferred by up to 500ms when under load
 	 */
-	public static final TaskPriority LOW = new TaskPriority(10000);
+	public static final TaskPriority NORMAL = MEDIUM;
+	/**
+	 * Priority for tasks which can be deferred by up to 1.5s when under load
+	 */
+	public static final TaskPriority LOW = new TaskPriority(1500);
+	/**
+	 * Priority for tasks which can be deferred by up to 10s when under load
+	 */
+	public static final TaskPriority LOWEST = new TaskPriority(10000);
 
 	private final long maxDeferred;
 	
