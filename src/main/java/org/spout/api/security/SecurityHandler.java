c74434911dc6f0cc6ec2296466b480437887ee5c
Raphfrk <raphfrk@gmail.com>
2012-06-09 13:26:41 +0100

    Changed from JCE to bouncy castle API

--- src/main/java/org/spout/api/security/SecurityHandler.java
+++ src/main/java/org/spout/api/security/SecurityHandler.java
@@ -26,24 +26,44 @@
  */
 package org.spout.api.security;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
+import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.Provider;
 import java.security.SecureRandom;
 import java.security.Security;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
-import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
-
+import org.bouncycastle.asn1.ASN1EncodableVector;
+import org.bouncycastle.asn1.ASN1Integer;
+import org.bouncycastle.asn1.DERNull;
+import org.bouncycastle.asn1.DERSequence;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.RSAPublicKeyStructure;
+import org.bouncycastle.crypto.AsymmetricBlockCipher;
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+import org.bouncycastle.crypto.AsymmetricCipherKeyPairGenerator;
+import org.bouncycastle.crypto.CipherParameters;
+import org.bouncycastle.crypto.InvalidCipherTextException;
+import org.bouncycastle.crypto.encodings.PKCS1Encoding;
+import org.bouncycastle.crypto.engines.RSAEngine;
+import org.bouncycastle.crypto.generators.RSAKeyPairGenerator;
+import org.bouncycastle.crypto.params.RSAKeyGenerationParameters;
+import org.bouncycastle.crypto.params.RSAKeyParameters;
+import org.bouncycastle.jcajce.provider.asymmetric.util.KeyUtil;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.spout.api.Spout;
 
 public class SecurityHandler {
+	
+	public static final boolean DECRYPT_MODE = false;
+	public static final boolean ENCRYPT_MODE = false;
+	
 
-	private static final ConcurrentHashMap<String, KeyPair> serverKeys = new ConcurrentHashMap<String, KeyPair>();
+	private static final ConcurrentHashMap<String, AsymmetricCipherKeyPair > serverKeys = new ConcurrentHashMap<String, AsymmetricCipherKeyPair >();
 	private static final Provider provider;
 	private static final SecurityHandler instance;
 	
@@ -64,40 +84,108 @@ public class SecurityHandler {
 		return instance;
 	}
 	
-	public Provider getProvider() {
-		return provider;
+	public byte[] encodeKey(CipherParameters key) {
+		if (key instanceof RSAKeyParameters) {
+			if (((RSAKeyParameters) key).isPrivate()) {
+				return null;
+			} else {
+				RSAKeyParameters rsaKey = (RSAKeyParameters) key;
+
+				ASN1EncodableVector encodable = new ASN1EncodableVector();
+				encodable.add(new ASN1Integer(rsaKey.getModulus()));
+				encodable.add(new ASN1Integer(rsaKey.getExponent()));
+
+				return KeyUtil.getEncodedSubjectPublicKeyInfo(
+						new AlgorithmIdentifier(PKCSObjectIdentifiers.rsaEncryption, new DERNull()), 
+						new DERSequence(encodable));
+			}
+		}
+		return null;
 	}
 	
-	public Cipher getCipher(String transformation) {
-		if (provider == null) {
-			return null;
+	public AsymmetricBlockCipher getCipher(String cipher, String padding) {
+		if (cipher.equals("RSA")) {
+			return addPadding(new RSAEngine(), padding);
 		} else {
-			try {
-				return Cipher.getInstance(transformation, provider);
-			} catch (NoSuchAlgorithmException e) {
-				Spout.getLogger().info("Unable to find algorithm " + transformation);
-			} catch (NoSuchPaddingException e) {
-				Spout.getLogger().info("Padding not found");
-			}
+			return null;
+		}
+	}
+	
+	private AsymmetricBlockCipher addPadding(AsymmetricBlockCipher rawCipher, String padding) {
+		if (padding == null) {
+			return rawCipher;
+		} else if (padding.equals("PKCS1")) {
+			return new PKCS1Encoding(rawCipher);
+		} else {
+			return null;
+		}
+	}
+	
+	public AsymmetricCipherKeyPairGenerator getGenerator(String algorithm) {
+		if (algorithm.equals("RSA")) {
+			return new RSAKeyPairGenerator();
+		} else {
+			Spout.getLogger().info("Unable to find key generator " + algorithm);
 			return null;
 		}
 	}
 
-	public KeyPair getKeyPair(String algorithm) {
+	public void initGenerator(int keySize, String algorithm, AsymmetricCipherKeyPairGenerator generator, SecureRandom random) {
+		if (algorithm.equals("RSA")) {
+			RSAKeyGenerationParameters params = new RSAKeyGenerationParameters(
+					new BigInteger("10001", 16), random, keySize, 80);
+			generator.init(params);
+		}
+	}
+
+	public byte[] processAll(AsymmetricBlockCipher cipher, byte[] input) {
+		int outputSize = 0;
+		int blockSize = cipher.getInputBlockSize();
+		List<byte[]> outputBlocks = new LinkedList<byte[]>();
+
+		int pos = 0;
+
+		while (pos < input.length) {
+			int length = Math.min(input.length - pos, blockSize);
+			System.out.println("length: " + length);
+			byte[] result;
+			try {
+				result = cipher.processBlock(input, pos, length);
+			} catch (InvalidCipherTextException e) {
+				Spout.getLogger().info("Error processing encrypted data");
+				return null;
+			}
+			outputSize += result.length;
+			outputBlocks.add(result);
+			pos += length;
+		}
+		
+		byte[] output = new byte[outputSize];
+		
+		pos = 0;
+		for (byte[] block : outputBlocks) {
+			System.out.println("block : " + block.length);
+			System.arraycopy(block, 0, output, pos, block.length);
+			pos += block.length;
+		}
+		
+		return output;
+	}
+
+	public AsymmetricCipherKeyPair  getKeyPair(String algorithm) {
 		return getKeyPair(1024, algorithm);
 	}
 
-	public KeyPair getKeyPair(int keySize, String algorithm) {
+	public AsymmetricCipherKeyPair  getKeyPair(int keySize, String algorithm) {
 		return getKeyPair(keySize, algorithm, "SHA1PRNG", "SUN");
 	}
 
-	public KeyPair getKeyPair(int keySize, String algorithm, String RNGAlgorithm, String RNGProvider) {
-		KeyPair pair = serverKeys.get(algorithm);
+	public AsymmetricCipherKeyPair  getKeyPair(int keySize, String algorithm, String RNGAlgorithm, String RNGProvider) {
+		AsymmetricCipherKeyPair pair = serverKeys.get(algorithm);
 		if (pair == null && provider != null) {
 			SecureRandom secureRandom;
 			try {
 				secureRandom = SecureRandom.getInstance(RNGAlgorithm, RNGProvider);
-				secureRandom.nextBytes(new byte[1]);
 			} catch (NoSuchProviderException e) {
 				Spout.getLogger().info("Unable to find algorithm to for random number generator");
 				return null;
@@ -105,27 +193,24 @@ public class SecurityHandler {
 				Spout.getLogger().info("Unable to find algorithm to generate random number generator for key pair creation");
 				return null;
 			}
-
-			KeyPairGenerator generator;
-			try {
-				generator = KeyPairGenerator.getInstance(algorithm, provider);
-			} catch (NoSuchAlgorithmException e) {
-				Spout.getLogger().info("Unable to find algorithm to generate key pair");
+			
+			AsymmetricCipherKeyPairGenerator generator = getGenerator(algorithm);
+			
+			if (generator == null) {
 				return null;
 			}
+			
+			initGenerator(keySize, algorithm, generator, secureRandom);
+			
+			AsymmetricCipherKeyPair  newPair = generator.generateKeyPair();
 
-			generator.initialize(keySize, secureRandom);
-
-			KeyPair newPair = generator.generateKeyPair();
-
-			KeyPair oldPair = serverKeys.putIfAbsent(algorithm, newPair);
+			AsymmetricCipherKeyPair  oldPair = serverKeys.putIfAbsent(algorithm, newPair);
 
 			if (oldPair == null) {
 				return newPair;
 			} else {
 				return oldPair;
 			}
-
 		} else {
 			return pair;
 		}
