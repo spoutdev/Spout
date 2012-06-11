74e8c077724bdb578c20485d67f2ac2819e915b0
Raphfrk <raphfrk@gmail.com>
2012-06-11 21:25:53 +0100

    Extra tick stages

--- src/main/java/org/spout/api/scheduler/TickStage.java
+++ src/main/java/org/spout/api/scheduler/TickStage.java
@@ -51,27 +51,55 @@ public class TickStage {
 	 * This is the second and subsequent stages of the tick
 	 */
 	public final static int STAGE2P = 1 << 2;
+	
+	/**
+	 * This is the final stage before entering the pre-snapshot stage.<br>
+	 * <br>
+	 * This is for minor changes prior to the snapshot process.
+	 */
+	public final static int PHYSICS = 1 << 3;
+	
+	/**
+	 * This is the final stage before entering the pre-snapshot stage.<br>
+	 * <br>
+	 * This is for minor changes prior to the snapshot process.
+	 */
+	public final static int GLOBAL_PHYSICS = 1 << 4;
+	
+	/**
+	 * This is the final stage before entering the pre-snapshot stage.<br>
+	 * <br>
+	 * This is for minor changes prior to the snapshot process.
+	 */
+	public final static int DYNAMIC_BLOCKS = 1 << 5;
+	
+	/**
+	 * This is the final stage before entering the pre-snapshot stage.<br>
+	 * <br>
+	 * This is for minor changes prior to the snapshot process.
+	 */
+	public final static int GLOBAL_DYNAMIC_BLOCKS = 1 << 6;
 
 	/**
 	 * This is the final stage before entering the pre-snapshot stage.<br>
 	 * <br>
 	 * This is for minor changes prior to the snapshot process.
 	 */
-	public final static int FINALIZE = 1 << 3;
+	public final static int FINALIZE = 1 << 7;
 
 	/**
 	 * This stage occurs before the snapshot stage.<br>
 	 * <br>
 	 * This is a MONITOR ONLY stage, no changes should be made during the stage.
 	 */
-	public final static int PRESNAPSHOT = 1 << 4;
+	public final static int PRESNAPSHOT = 1 << 8;
 
 	/**
 	 * This is the snapshot copy stage.<br>
 	 * <br>
 	 * All snapshots are updated to the equal to the live value.
 	 */
-	public final static int SNAPSHOT = 1 << 5;
+	public final static int SNAPSHOT = 1 << 9;
 
 	public static String getStage(int num) {
 		switch (num) {
@@ -82,10 +110,18 @@ public class TickStage {
 			case 1 << 2:
 				return "STAGE2P";
 			case 1 << 3:
-				return "FINALIZE";
+				return "PHYSICS";
 			case 1 << 4:
-				return "PRESNAPSHOT";
+				return "GLOBAL_PHYSICS";
 			case 1 << 5:
+				return "DYNAMIC_BLOCKS";
+			case 1 << 6:
+				return "GLOBAL_DYNAMIC_BLOCKS";
+			case 1 << 7:
+				return "FINALIZE";
+			case 1 << 8:
+				return "PRESNAPSHOT";
+			case 1 << 9:
 				return "SNAPSHOT";
 			default:
 				return "UNKNOWN";
