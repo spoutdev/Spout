1a8f84402b08fec49b8e97913f76f10433eaff99
Ian Macalinao <ianmacalinao@gmail.com>
2012-08-06 01:58:23 -0500

    Added WAV file support and a prototype Sound manager.
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/main/java/org/spout/engine/filesystem/ClientFileSystem.java
+++ src/main/java/org/spout/engine/filesystem/ClientFileSystem.java
@@ -29,6 +29,7 @@ package org.spout.engine.filesystem;
 import org.spout.engine.resources.loader.MeshLoader;
 import org.spout.engine.resources.loader.RenderMaterialLoader;
 import org.spout.engine.resources.loader.ShaderLoader;
+import org.spout.engine.resources.loader.SoundWavLoader;
 import org.spout.engine.resources.loader.TextureLoader;
 
 public class ClientFileSystem extends SharedFileSystem {
@@ -49,6 +50,7 @@ public class ClientFileSystem extends SharedFileSystem {
 		registerLoader("mesh", new MeshLoader());
 		registerLoader("material", new RenderMaterialLoader());
 
+		registerLoader("sound_wav", new SoundWavLoader());
 		//registerLoader("font", new FontLoader());
 		
 		
