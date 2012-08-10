5f0e1ae23aadad39542a9d607060ecdd0373083e
zml2008 <zach@zachsthings.com>
2012-08-09 21:24:20 -0700

    Updated ConfigurationMigrator to allow migrating values

--- src/main/java/org/spout/api/util/config/ConfigurationMigrator.java
+++ src/main/java/org/spout/api/util/config/ConfigurationMigrator.java
@@ -27,66 +27,132 @@
 package org.spout.api.util.config;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 
+import org.apache.commons.lang3.StringUtils;
 import org.spout.api.exception.ConfigurationException;
-import org.spout.api.util.config.Configuration;
-import org.spout.api.util.config.ConfigurationNode;
 
 /**
  * A simple migrator for configurations that moves values from one key to another.
- * Values do not have their types converted.
+ * It can also convert values
  */
 public abstract class ConfigurationMigrator {
-    protected final Configuration config;
-    protected final File oldFile;
+    private final Configuration configuration;
 
-    protected ConfigurationMigrator(File configFile, Configuration processor) {
-        this.oldFile = configFile;
-        this.config = processor;
+    protected ConfigurationMigrator(Configuration configuration) {
+        this.configuration = configuration;
     }
 
-    protected Map<String[], String[]> getMigrationKeys() {
-		Map<String, String> joined = getJoinedMigrationKeys();
-		if (joined == null) {
-			return null;
-		}
-		Map<String[], String[]> ret = new HashMap<String[], String[]>();
-		for (Map.Entry<String, String> entry : joined.entrySet()) {
-			String[] newKey = config.getPathSeparatorPattern().split(entry.getKey());
-			String[] newValue = config.getPathSeparatorPattern().split(entry.getValue().replaceAll("%", Matcher.quoteReplacement(entry.getKey())));
-			ret.put(newKey,  newValue);
-		}
-		return ret;
+    protected abstract Map<String[], MigrationAction> getMigrationActions();
+
+	public Configuration getConfiguration() {
+		return configuration;
 	}
 
-	protected Map<String, String> getJoinedMigrationKeys() {
-		return null;
+	/**
+	 * This implementation of MigrationAction converts configuration keys based on predefined
+	 * stringpath patterns where % is replaced by the old key. The stringpath pattern is split by the configuration's path separator
+	 *
+	 * @see org.spout.api.util.config.Configuration#getPathSeparator()
+	 */
+	public final class NewJoinedKey implements MigrationAction {
+		private final String newKeyPattern;
+
+		public NewJoinedKey(String newKeyPattern) {
+			this.newKeyPattern = newKeyPattern;
+		}
+
+		public String[] convertKey(String[] key) {
+			String oldKey = StringUtils.join(key, getConfiguration().getPathSeparator());
+			return configuration.getPathSeparatorPattern().split(newKeyPattern.replaceAll("%", Matcher.quoteReplacement(oldKey)));
+		}
+
+		public Object convertValue(Object value) {
+			return value;
+		}
 	}
 
+	/**
+	 * This implementation of MigrationAction changes the key of a configuration value to a predefined new key
+	 */
+	public static final class NewKey implements MigrationAction {
+		private final String[] newKey;
+
+		public NewKey(String... key) {
+			this.newKey = key;
+		}
+
+		public String[] convertKey(String[] key) {
+			return newKey;
+		}
+
+		public Object convertValue(Object value) {
+			return value;
+		}
+	}
+
+	/**
+	 * Represents the two sides of migrating an existing configuration key:
+	 * Converting the key and converting the value
+	 */
+	public static interface MigrationAction {
+		/**
+		 * This method converts the old configuration key to its migrated value.
+		 *
+		 * @param key The existing configuration key
+		 * @return The key modified to its new value
+		 */
+		public String[] convertKey(String[] key);
+		/**
+		 * This method converts the old configuration value to its migrated value.
+		 *
+		 * @param value The existing configuration value
+		 * @return The value modified to its new value
+		 */
+		public Object convertValue(Object value);
+	}
+
+	/**
+	 * This method checks whether migration is needed on the {@link Configuration} this instance is constructed with
+	 *
+	 * @return Whether migration is needed
+	 */
     protected abstract boolean shouldMigrate();
 
+	/**
+	 * Perform migration of the configuration this object was constructed with
+	 * If migration was not necessary ({@link #shouldMigrate()} returned false), the method invocation will be considered successful.
+	 * If {@link #configuration} is a {@link FileConfiguration}, the file the configuration vas previously stored in will be
+	 * moved to (file name).old as a backup of the data before migration
+	 *
+	 * @return Null if successful, an error message if an error occurred.
+	 */
     public String migrate() {
         if (!shouldMigrate()) {
             return null;
         }
 
-        if (!oldFile.renameTo(new File(oldFile.getAbsolutePath() + ".old"))) {
-            return "Unable to rename backup old configuration file!";
-        }
-        for (Map.Entry<String[], String[]> entry : getMigrationKeys().entrySet()) {
-			ConfigurationNode existingNode = config.getNode(entry.getKey());
-            Object existing = existingNode.getValue();
+		if (configuration instanceof FileConfiguration) {
+			File oldFile = ((FileConfiguration) configuration).getFile();
+        	if (!oldFile.renameTo(new File(oldFile.getAbsolutePath() + ".old"))) {
+            	return "Unable to rename backup old configuration file!";
+        	}
+		}
+
+        for (Map.Entry<String[], MigrationAction> entry : getMigrationActions().entrySet()) {
+			final ConfigurationNode existingNode = configuration.getNode(entry.getKey());
+            final Object existing = existingNode.getValue();
             existingNode.remove();
             if (existing == null || entry.getValue() == null) {
                 continue;
             }
-            config.getNode(entry.getValue()).setValue(existing);
+			final String[] newKey = entry.getValue().convertKey(entry.getKey());
+			final Object newValue = entry.getValue().convertValue(existing);
+            configuration.getNode(newKey).setValue(newValue);
         }
         try {
-			config.save();
+			configuration.save();
 		} catch (ConfigurationException e) {
 			return e.getMessage();
 		}
