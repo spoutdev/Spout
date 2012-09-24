c24acc9e857e8a4d68df736a2367d4d15c4c8afc
RoyAwesome <madKylegar@gmail.com>
2012-09-24 11:27:37 -0700

    In progress 2d sprite rendering.
    Gotta set up the camera and this is almost done.

---
+++ src/main/java/org/spout/engine/batcher/SpriteBatch.java
@@ -0,0 +1,93 @@
+/*
+ * This file is part of Spout.
+ *
+ * Copyright (c) 2011-2012, SpoutDev <http://www.spout.org/>
+ * Spout is licensed under the SpoutDev License Version 1.
+ *
+ * Spout is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * In addition, 180 days after any changes are published, you can use the
+ * software, incorporating those changes, under the terms of the MIT license,
+ * as described in the SpoutDev License Version 1.
+ *
+ * Spout is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License,
+ * the MIT license and the SpoutDev License Version 1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
+ * License and see <http://www.spout.org/SpoutDevLicenseV1.txt> for the full license,
+ * including the MIT license.
+ */
+package org.spout.engine.batcher;
+
+import java.util.ArrayList;
+
+import org.lwjgl.opengl.GL11;
+import org.spout.api.render.RenderMaterial;
+import org.spout.api.render.Renderer;
+import org.spout.engine.renderer.BatchVertexRenderer;
+
+public class SpriteBatch {
+	private class TextureRectangle {
+		public float x,y,w,h;
+		public RenderMaterial material;
+	}
+	
+	
+	Renderer renderer;
+	ArrayList<TextureRectangle> sprites = new ArrayList<TextureRectangle>();
+	
+	public SpriteBatch() {
+		this.renderer = BatchVertexRenderer.constructNewBatch(GL11.GL_TRIANGLES);
+	}
+	
+	public void begin() {
+		
+	}
+	
+	public void render() {
+		renderer.begin();
+		for(int i = 0; i < sprites.size(); i++) {
+			TextureRectangle rect = sprites.get(i);
+			renderer.addVertex(rect.x, rect.y);
+			renderer.addTexCoord(0, 0);
+			renderer.addVertex(rect.x + rect.w, rect.y);
+			renderer.addTexCoord(1, 0);
+			renderer.addVertex(rect.x, rect.y + rect.h);
+			renderer.addTexCoord(0, 1);
+			
+			renderer.addVertex(rect.x + rect.w, rect.y);
+			renderer.addTexCoord(1, 0);
+			renderer.addVertex(rect.x + rect.w, rect.y + rect.h);
+			renderer.addTexCoord(1, 1);
+			renderer.addVertex(rect.x, rect.y + rect.h);
+			renderer.addTexCoord(0, 1);	
+			
+		}
+		renderer.end();
+		
+		for(int i = 0; i < sprites.size(); i++) {
+			TextureRectangle rect = sprites.get(i);
+			//TODO: set up camera here for the material
+			renderer.render(rect.material, (i * 6), (i * 6) + 5);
+		}
+	}
+	
+	public void draw(RenderMaterial material, float x, float y, float w, float h) {
+		TextureRectangle rect = new TextureRectangle();
+		rect.x = x;
+		rect.y = y;
+		rect.w = w;
+		rect.h = h;
+		rect.material = material;
+		sprites.add(rect);
+	}
+	
+	
+}
