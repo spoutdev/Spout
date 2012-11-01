8b806783781b14fc226c9f82021e55ed210a6067
zml2008 <zach@zachsthings.com>
2012-11-01 14:16:44 -0700

    Remove old NBT transform format from NBTMapper

--- src/main/java/org/spout/api/util/NBTMapper.java
+++ src/main/java/org/spout/api/util/NBTMapper.java
@@ -43,55 +43,6 @@ import org.spout.nbt.Tag;
  * Utility class to map out complex objects into NBT structures vice-versa.
  */
 public class NBTMapper {
-	/**
-	 * This method takes a list and constructs a transform from it.
-	 * @param list the list of floats representing a transform.
-	 * @return the constructed transform from NBT
-	 */
-	public static Transform nbtToTransform(World world, List<? extends FloatTag> list) {
-		//Position
-		float px = SafeCast.toFloat(list.get(0).getValue(), 1f);
-		float py = SafeCast.toFloat(list.get(1).getValue(), 85f);
-		float pz = SafeCast.toFloat(list.get(2).getValue(), 1f);
-		//Rotation
-		float rw = SafeCast.toFloat(list.get(3).getValue(), 1f);
-		float rx = SafeCast.toFloat(list.get(4).getValue(), 1f);
-		float ry = SafeCast.toFloat(list.get(5).getValue(), 1f);
-		float rz = SafeCast.toFloat(list.get(6).getValue(), 1f);
-		//Scale
-		float sx = SafeCast.toFloat(list.get(7).getValue(), 1f);
-		float sy = SafeCast.toFloat(list.get(8).getValue(), 1f);
-		float sz = SafeCast.toFloat(list.get(9).getValue(), 1f);
-		return new Transform(new Point(world, px, py, pz), new Quaternion(rw, rx, ry, rz), new Vector3(sx, sy, sz));
-	}
-
-	/**
-	 * This method takes a transform and converts it into a series of float tags representing a transform. The world object
-	 * within the position object composed in this transform will not be saved.
-	 * @param transform The transform to convert to tags
-	 * @return list composed of float tags representing the transform
-	 */
-	public static List<FloatTag> transformToNBT(Transform transform) {
-		ArrayList<FloatTag> list = new ArrayList<FloatTag>(10);
-		Vector3 point = transform.getPosition();
-		Quaternion rotation = transform.getRotation();
-		Vector3 scale = transform.getScale();
-		//Position
-		list.add(new FloatTag("px", point.getX()));
-		list.add(new FloatTag("py", point.getY()));
-		list.add(new FloatTag("pz", point.getZ()));
-		//Rotation
-		list.add(new FloatTag("rw", rotation.getW()));
-		list.add(new FloatTag("rx", rotation.getX()));
-		list.add(new FloatTag("ry", rotation.getY()));
-		list.add(new FloatTag("rz", rotation.getZ()));
-		//Scale
-		list.add(new FloatTag("sx", scale.getX()));
-		list.add(new FloatTag("sy", scale.getY()));
-		list.add(new FloatTag("sz", scale.getZ()));
-		//Return list of floats
-		return list;
-	}
 
 	/**
 	 * Takes in an NBT tag, sanely checks null status, and then returns its value.
@@ -105,11 +56,11 @@ public class NBTMapper {
 			return t.getValue();
 		}
 	}
-	
+
 	/**
 	 * Takes in an NBT tag, sanely checks null status, and then returns it value.
 	 * This method will return null if the value cannot be cast to the given class.
-	 * 
+	 *
 	 * @param t Tag to get the value from
 	 * @param clazz the return type to use
 	 * @return the value as an onbject of the same type as the given class
@@ -125,11 +76,11 @@ public class NBTMapper {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Takes in an NBT tag, sanely checks null status, and then returns it value.
 	 * This method will return null if the value cannot be cast to the default value.
-	 * 
+	 *
 	 * @param t Tag to get the value from
 	 * @param defaultValue the value to return if the tag or its value is null or the value cannot be cast
 	 * @return the value as an onbject of the same type as the default value, or the default value
@@ -146,6 +97,6 @@ public class NBTMapper {
 			return defaultValue;
 		}
 	}
-	
+
 
 }
