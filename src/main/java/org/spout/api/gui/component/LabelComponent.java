6fc8b74dc078ac61fd83bf7141d5ebaa2bc75324
Karang <arthur.hennequin@free.fr>
2012-11-04 18:44:58 +0100

    Fix ChatArguments render Signed-of-by : Karang <arthur.hennequin@free.fr>

--- src/main/java/org/spout/api/gui/component/LabelComponent.java
+++ src/main/java/org/spout/api/gui/component/LabelComponent.java
@@ -33,8 +33,8 @@ import java.util.List;
 import org.spout.api.Client;
 import org.spout.api.Spout;
 import org.spout.api.chat.ChatArguments;
-import org.spout.api.chat.style.ChatStyle;
 import org.spout.api.chat.style.ColorChatStyle;
+import org.spout.api.chat.style.ResetChatStyle;
 import org.spout.api.component.components.WidgetComponent;
 import org.spout.api.gui.render.RenderPart;
 import org.spout.api.map.DefaultedKey;
@@ -86,11 +86,9 @@ public class LabelComponent extends WidgetComponent {
 		if (getFont() == null) {
 			return ret;
 		}
-
-		// TODO: Make this work with ChatStyles
-		/*Color color = getColor();
+		
+		Color color = getColor();
 		Font font = getFont();
-		boolean skipChar = false;
 
 		float w = font.getWidth();
 		float h = font.getHeight();
@@ -101,43 +99,36 @@ public class LabelComponent extends WidgetComponent {
 		float screenWidth = ((Client) Spout.getEngine()).getResolution().getX();
 		float screenHeight = ((Client) Spout.getEngine()).getResolution().getY();
 
-		for (int i = 0; i < getText().length(); i++) {
-			if (skipChar) {
-				skipChar = false;
-				continue;
-			}
-			char c = getText().charAt(i);
-			if (c == ' ') {
-				xCursor += font.getSpaceWidth() / screenWidth;
-			} else if (c == '\n') {
-				xCursor = getOwner().getGeometry().getX();
-				yCursor -= font.getCharHeight() / screenHeight;
-			} else if (c == '§') {
-				if (i + 1 == getText().length()) {
-					continue;
-				}
-				ChatStyle style = ChatStyle.byCode(getText().charAt(i + 1));
-				skipChar = true;
-				if (style != null) {
-					if (style instanceof ColorChatStyle) {
-						color = ((ColorChatStyle) style).getColor();
+		for (Object arg : getText().getArguments()) {
+			if (arg instanceof String) {
+				String txt = (String) arg;
+				for (int i=0 ; i<txt.length() ; i++) {
+					char c = txt.charAt(i);
+					if (c==' ') {
+						xCursor += font.getSpaceWidth() / screenWidth;
+					} else if (c == '\n') {
+						xCursor = getOwner().getGeometry().getX();
+						yCursor -= font.getCharHeight() / screenHeight;
+					} else {
+						java.awt.Rectangle r = font.getPixelBounds(c);
+
+						RenderPart part = new RenderPart();
+						part.setRenderMaterial(font.getMaterial());
+						part.setColor(color);
+						part.setSprite(new Rectangle(xCursor, yCursor, (float) r.width / screenWidth, h / screenHeight));
+						part.setSource(new Rectangle(r.x / w, 0f, r.width / w, 1f));
+
+						xCursor += (float) font.getAdvance(c) / screenWidth;
+
+						ret.add(part);
 					}
-					// TODO: Other chat styles
 				}
-			} else {
-				java.awt.Rectangle r = font.getPixelBounds(c);
-
-				RenderPart part = new RenderPart();
-				part.setRenderMaterial(font.getMaterial());
-				part.setColor(color);
-				part.setSprite(new Rectangle(xCursor, yCursor, (float) r.width / screenWidth, h / screenHeight));
-				part.setSource(new Rectangle(r.x / w, 0f, r.width / w, 1f));
-
-				xCursor += (float) font.getAdvance(c) / screenWidth;
-
-				ret.add(part);
+			} else if (arg instanceof ColorChatStyle) {
+				color = ((ColorChatStyle) arg).getColor();
+			} else if (arg instanceof ResetChatStyle) {
+				color = Color.black;
 			}
-		}*/
+		}
 
 		return ret;
 	}
@@ -152,13 +143,12 @@ public class LabelComponent extends WidgetComponent {
 	}
 
 	public ChatArguments getText() {
-		//return getData().get(KEY_TEXT);
-		return new ChatArguments("Not yet implemented!");
+		return getData().get(KEY_TEXT);
 	}
 
 	public void setText(ChatArguments text) {
-		/*getData().put(KEY_TEXT, text);
-		getOwner().update();*/
+		getData().put(KEY_TEXT, text);
+		getOwner().update();
 	}
 
 	public Color getColor() {
