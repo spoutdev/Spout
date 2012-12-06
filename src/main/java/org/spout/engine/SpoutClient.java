83526a0c664302487389a669ca46d66b51c6c01a
Chris Sanders <zidane@outlook.com>
2012-12-05 23:21:02 -0600

    Added toggle physics command.
    - r_noclip is the command. It will give the active player physics so
      people can get an initial look at client side physics. You can't move in
      this mode as input is overriden by physics.
    
      Want to fix that @Raphfrk :p?
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spout/engine/SpoutClient.java
+++ src/main/java/org/spout/engine/SpoutClient.java
@@ -68,6 +68,7 @@ import org.spout.api.command.annotated.AnnotatedCommandRegistrationFactory;
 import org.spout.api.command.annotated.SimpleInjector;
 import org.spout.api.component.components.CameraComponent;
 import org.spout.api.component.components.HitBlockComponent;
+import org.spout.api.component.components.PhysicsComponent;
 import org.spout.api.component.components.PredictableTransformComponent;
 import org.spout.api.datatable.SerializableMap;
 import org.spout.api.entity.Entity;
