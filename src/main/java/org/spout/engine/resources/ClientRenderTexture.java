8f2f5488800ce585b61aa0ee1b1a53d7b78d2cb2
RoyAwesome <madKylegar@gmail.com>
2012-12-12 00:57:34 -0800

    Working on RenderTexture

--- src/main/java/org/spout/engine/resources/ClientRenderTexture.java
+++ src/main/java/org/spout/engine/resources/ClientRenderTexture.java
@@ -27,23 +27,28 @@
 package org.spout.engine.resources;
 
 import java.awt.Color;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.GL32;
 import org.spout.api.Spout;
 import org.spout.api.render.RenderMode;
 import org.spout.api.render.Texture;
 import org.spout.engine.SpoutClient;
 
-public class ClientRenderTexture extends Texture {
+public class ClientRenderTexture extends ClientTexture {
 	public static final int INVALID_BUFFER = -1;
-	public static final int OUTPUT_BUFFER = 0;
+	public static final int SCREEN_BUFFER = 0;
+	public static ByteBuffer EMPTY_BUFFER = BufferUtils.createByteBuffer(0);
 	
 	private int framebuffer = INVALID_BUFFER;
 	private boolean useDepthBuffer = false;
 	private boolean useStencilBuffer = false;
 	
-	int colorTarget = INVALID_BUFFER;
 	int depthTarget = INVALID_BUFFER;
 	int stencilTarget = INVALID_BUFFER; //TODO: Implement stencil component
 
@@ -66,7 +71,17 @@ public class ClientRenderTexture extends Texture {
 	public void activate() {
 		if(isGL30()){
 			if(framebuffer == INVALID_BUFFER) return; //Can't set this to active if it's not created yet
-			GL30.glBindRenderbuffer(GL30.GL_FRAMEBUFFER, framebuffer);
+			GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, framebuffer);
+			
+		}
+	}
+	
+	public void release() {
+		if(isGL30()) {
+			if(framebuffer != INVALID_BUFFER) {
+				GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, SCREEN_BUFFER);
+				
+			}
 		}
 	}
 
@@ -75,15 +90,22 @@ public class ClientRenderTexture extends Texture {
 		if(isGL30()) {
 			if(framebuffer == INVALID_BUFFER) {
 				framebuffer = GL30.glGenFramebuffers();
+				GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, framebuffer);
 				
 				//Create the color buffer for this renderTexture
-				colorTarget = GL11.glGenTextures();
-				GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, framebuffer);
-				GL11.glBindTexture(GL11.GL_TEXTURE_2D, colorTarget);
+				textureID = GL11.glGenTextures();
+				GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
 				
-				GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA8, this.getWidth(), this.getHeight(), 0, GL11.GL_RGBA8, GL11.GL_UNSIGNED_BYTE, 0);
+				GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA8, this.getWidth(), this.getHeight(), 0, GL11.GL_RGBA8, GL11.GL_UNSIGNED_BYTE, EMPTY_BUFFER);
 				GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
 				GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
+				
+				GL32.glFramebufferTexture(GL30.GL_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT0, 0, textureID);
+				
+				IntBuffer bufs = BufferUtils.createIntBuffer(1);
+				bufs.put(GL30.GL_COLOR_ATTACHMENT0);
+				bufs.flip();
+				GL20.glDrawBuffers(bufs);
 				
 				if(useDepthBuffer) {
 					depthTarget = GL30.glGenRenderbuffers();
@@ -91,26 +113,14 @@ public class ClientRenderTexture extends Texture {
 					GL30.glRenderbufferStorage(GL30.GL_RENDERBUFFER, GL11.GL_DEPTH_COMPONENT, this.getWidth(), this.getHeight());
 					GL30.glFramebufferRenderbuffer(GL30.GL_FRAMEBUFFER, GL30.GL_DEPTH_ATTACHMENT, GL30.GL_RENDERBUFFER, depthTarget);
 				}
-				GL30.glFramebufferTexture2D(GL30.GL_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT0, 0, colorTarget, 0);
-				
+				GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, SCREEN_BUFFER);
+				GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
 			}
 		}
 		
 	}
 
-	@Override
-	public void bind() {
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, colorTarget);
-		
-	}
 
-	public void release() {
-		if(isGL30()) {
-			if(framebuffer != INVALID_BUFFER) {
-				GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, OUTPUT_BUFFER);
-			}
-		}
-	}
 	
 	protected boolean isGL30() {
 		return ((SpoutClient)Spout.getEngine()).getRenderMode() == RenderMode.GL30;
