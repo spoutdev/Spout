b0bea25e8bf4a219a7d7acd7430aa8adf399143f
Chris Sanders <zidane@outlook.com>
2013-01-04 21:34:13 -0600

    BlockMaterials have high friction/no energy.
    - By default, can be changed.
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spout/api/material/BlockMaterial.java
+++ src/main/java/org/spout/api/material/BlockMaterial.java
@@ -75,25 +75,29 @@ public class BlockMaterial extends Material implements Placeable {
 	public BlockMaterial(short dataMask, String name, String model){
 		super(dataMask, name, model);
 		collisionObject.setCollisionShape(new BoxShape(1F, 1F, 1F));
-		collisionObject.setRestitution(1f);
+		collisionObject.setRestitution(0f);
+		collisionObject.setFriction(1f);
 	}
 
 	public BlockMaterial(String name, int data, Material parent, String model) {
 		super(name, data, parent, model);
 		collisionObject.setCollisionShape(new BoxShape(1F, 1F, 1F));
-		collisionObject.setRestitution(1f);
+		collisionObject.setRestitution(0f);
+		collisionObject.setFriction(1f);
 	}
 
 	protected BlockMaterial(String name, short id) {
 		super(name, id);
 		collisionObject.setCollisionShape(new BoxShape(1F, 1F, 1F));
-		collisionObject.setRestitution(1f);
+		collisionObject.setRestitution(0f);
+		collisionObject.setFriction(1f);
 	}
 
 	protected BlockMaterial(String name) {
 		super(name);
 		collisionObject.setCollisionShape(new BoxShape(1F, 1F, 1F));
-		collisionObject.setRestitution(1f);
+		collisionObject.setRestitution(0f);
+		collisionObject.setFriction(1f);
 	}
 
 	/**
