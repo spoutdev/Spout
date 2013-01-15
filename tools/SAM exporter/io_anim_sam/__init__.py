de4df420d626e0c4840a2cccbe516a05555bb626
L5D <L5D@L5D-PC>
2013-01-15 21:38:51 +0100

    Update animation exporter for blender (not fully functionnal say Alkaelis)

--- tools/SAM exporter/io_anim_sam/__init__.py
+++ tools/SAM exporter/io_anim_sam/__init__.py
@@ -17,7 +17,7 @@ class ExportSAM(bpy.types.Operator, ExportHelper):
     bl_idname       = "export_anim.sam";
     bl_label        = "Spout Animation Model";
     bl_options      = {'PRESET'};
-
+    
     filename_ext    = ".sam";
 
     startFrame_prop = IntProperty(name="Start frame",
@@ -27,16 +27,44 @@ class ExportSAM(bpy.types.Operator, ExportHelper):
                                 description="Ending frame for the animation",
                                 default=250);
 
+    def mul(self, mat1, mat2):
+        return [[mat1[0][0]*mat2[0][0]+mat1[0][1]*mat2[1][0]+mat1[0][2]*mat2[2][0]+mat1[0][3]*mat2[3][0],
+                mat1[0][0]*mat2[0][1]+mat1[0][1]*mat2[1][1]+mat1[0][2]*mat2[2][1]+mat1[0][3]*mat2[3][1],
+                mat1[0][0]*mat2[0][2]+mat1[0][1]*mat2[1][2]+mat1[0][2]*mat2[2][2]+mat1[0][3]*mat2[3][2],
+                mat1[0][0]*mat2[0][3]+mat1[0][1]*mat2[1][3]+mat1[0][2]*mat2[2][3]+mat1[0][3]*mat2[3][3]],
+                [mat1[1][0]*mat2[0][0]+mat1[1][1]*mat2[1][0]+mat1[1][2]*mat2[2][0]+mat1[1][3]*mat2[3][0],
+                mat1[1][0]*mat2[0][1]+mat1[1][1]*mat2[1][1]+mat1[1][2]*mat2[2][1]+mat1[1][3]*mat2[3][1],
+                mat1[1][0]*mat2[0][2]+mat1[1][1]*mat2[1][2]+mat1[1][2]*mat2[2][2]+mat1[1][3]*mat2[3][2],
+                mat1[1][0]*mat2[0][3]+mat1[1][1]*mat2[1][3]+mat1[1][2]*mat2[2][3]+mat1[1][3]*mat2[3][3]],
+                
+                [mat1[2][0]*mat2[0][0]+mat1[2][1]*mat2[1][0]+mat1[2][2]*mat2[2][0]+mat1[2][3]*mat2[3][0],
+                 mat1[2][0]*mat2[0][1]+mat1[2][1]*mat2[1][1]+mat1[2][2]*mat2[2][1]+mat1[2][3]*mat2[3][1],
+                 mat1[2][0]*mat2[0][2]+mat1[2][1]*mat2[1][2]+mat1[2][2]*mat2[2][2]+mat1[2][3]*mat2[3][2],
+                 mat1[2][0]*mat2[0][3]+mat1[2][1]*mat2[1][3]+mat1[2][2]*mat2[2][3]+mat1[2][3]*mat2[3][3]],
+                
+                [mat1[3][0]*mat2[0][0]+mat1[3][1]*mat2[1][0]+mat1[3][2]*mat2[2][0]+mat1[3][3]*mat2[3][0],
+                 mat1[3][0]*mat2[0][1]+mat1[3][1]*mat2[1][1]+mat1[3][2]*mat2[2][1]+mat1[3][3]*mat2[3][1],
+                 mat1[3][0]*mat2[0][2]+mat1[3][1]*mat2[1][2]+mat1[3][2]*mat2[2][2]+mat1[3][3]*mat2[3][2],
+                 mat1[3][0]*mat2[0][3]+mat1[3][1]*mat2[1][3]+mat1[3][2]*mat2[2][3]+mat1[3][3]*mat2[3][3]]];
+
+        
+    def getMatrix(self, bone):
+        if (bone.parent is None):
+            return bone.matrix_channel;
+        else:
+            return self.mul(self.getMatrix(bone.parent), bone.matrix_channel);
+
     def writeAnimation(self, f, scene, startFrame, endFrame, bone):
         indent = "        ";
         for frame in range(startFrame, endFrame+1):
             scene.frame_set(frame);
-            m = bone.matrix_local;
+            rot = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+            m = self.mul(rot, self.getMatrix(bone));
             f.write(indent + str(frame) + ": ");
-            f.write("%.6f, %.6f, %.6f, %.6f, " % (m[0][0], m[0][1], m[0][2], m[0][3]));
-            f.write("%.6f, %.6f, %.6f, %.6f, " % (m[1][0], m[1][1], m[1][2], m[1][3]));
-            f.write("%.6f, %.6f, %.6f, %.6f, " % (m[2][0], m[2][1], m[2][2], m[2][3]));
-            f.write("%.6f, %.6f, %.6f, %.6f" % (m[3][0], m[3][1], m[3][2], m[3][3]));
+            f.write("%.6f, %.6f, %.6f, %.6f, " % (m[0][0], m[1][0], m[2][0], m[3][0]));
+            f.write("%.6f, %.6f, %.6f, %.6f, " % (m[0][1], m[1][1], m[2][1], m[3][1]));
+            f.write("%.6f, %.6f, %.6f, %.6f, " % (m[0][2], m[1][2], m[2][2], m[3][2]));
+            f.write("%.6f, %.6f, %.6f, %.6f" % (m[0][3], m[1][3], m[2][3], m[3][3]));
             f.write("\n");
 
     def saveSAM(self, context, path, startFrame, endFrame):
@@ -44,19 +72,20 @@ class ExportSAM(bpy.types.Operator, ExportHelper):
 
         scene = context.scene;
         obj = context.object;
-        armature = obj.parent.data;
+        armature = obj.parent;
 
+        f.write("Skeleton: skeleton://please edit path to skeleton.ske\n");
         f.write("frames: %d\n" % (endFrame - startFrame + 1));
         f.write("delay: %.6f\n" % (1.0 / (scene.render.fps / scene.render.fps_base)));
         f.write("bones_data:\n");
-
-        for bone in armature.bones:
+        
+        for bone in armature.pose.bones:
             f.write("    " + bone.name + ":\n");
             self.writeAnimation(f, scene, startFrame, endFrame, bone);
-
+        
         f.close();
         return {'FINISHED'};
-
+    
     def execute(self, context):
         path = self.as_keywords()["filepath"];
         start = self.as_keywords()["startFrame_prop"];
@@ -69,7 +98,7 @@ def menu_func(self, context):
 def register():
     bpy.utils.register_module(__name__);
     bpy.types.INFO_MT_file_export.append(menu_func);
-
+    
 def unregister():
     bpy.utils.unregister_module(__name__);
     bpy.types.INFO_MT_file_export.remove(menu_func);
