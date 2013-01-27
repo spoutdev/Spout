d7100ec518452f4446cb0959cf125314f0927135
Alkaelis <AlkaelisFenaro@gmail.com>
2013-01-27 20:37:13 +0100

    Animation exporter and cool new spouty.
    Signed-off-by: Alkaelis <AlkaelisFenaro@gmail.com>

--- tools/SAM exporter/io_anim_sam/__init__.py
+++ tools/SAM exporter/io_anim_sam/__init__.py
@@ -1,11 +1,16 @@
 import bpy;
-from bpy_extras.io_utils import ExportHelper;
-from bpy.props import IntProperty;
+from bpy_extras.io_utils import (ExportHelper,
+                                 axis_conversion,
+                                 );
+from bpy.props import (FloatProperty,
+                       EnumProperty,
+                       IntProperty,
+                       );
 from mathutils import Matrix;
 
 bl_info = {
     "name":         "Spout Animation Model",
-    "author":       "Karang",
+    "author":       "Karang, Alkaelis",
     "blender":      (2,6,2),
     "version":      (0,0,1),
     "location":     "File > Import-Export",
@@ -27,61 +32,67 @@ class ExportSAM(bpy.types.Operator, ExportHelper):
                                 description="Ending frame for the animation",
                                 default=250);
 
-    def mul(self, mat1, mat2):
-        return [[mat1[0][0]*mat2[0][0]+mat1[0][1]*mat2[1][0]+mat1[0][2]*mat2[2][0]+mat1[0][3]*mat2[3][0],
-                mat1[0][0]*mat2[0][1]+mat1[0][1]*mat2[1][1]+mat1[0][2]*mat2[2][1]+mat1[0][3]*mat2[3][1],
-                mat1[0][0]*mat2[0][2]+mat1[0][1]*mat2[1][2]+mat1[0][2]*mat2[2][2]+mat1[0][3]*mat2[3][2],
-                mat1[0][0]*mat2[0][3]+mat1[0][1]*mat2[1][3]+mat1[0][2]*mat2[2][3]+mat1[0][3]*mat2[3][3]],
-                [mat1[1][0]*mat2[0][0]+mat1[1][1]*mat2[1][0]+mat1[1][2]*mat2[2][0]+mat1[1][3]*mat2[3][0],
-                mat1[1][0]*mat2[0][1]+mat1[1][1]*mat2[1][1]+mat1[1][2]*mat2[2][1]+mat1[1][3]*mat2[3][1],
-                mat1[1][0]*mat2[0][2]+mat1[1][1]*mat2[1][2]+mat1[1][2]*mat2[2][2]+mat1[1][3]*mat2[3][2],
-                mat1[1][0]*mat2[0][3]+mat1[1][1]*mat2[1][3]+mat1[1][2]*mat2[2][3]+mat1[1][3]*mat2[3][3]],
-                
-                [mat1[2][0]*mat2[0][0]+mat1[2][1]*mat2[1][0]+mat1[2][2]*mat2[2][0]+mat1[2][3]*mat2[3][0],
-                 mat1[2][0]*mat2[0][1]+mat1[2][1]*mat2[1][1]+mat1[2][2]*mat2[2][1]+mat1[2][3]*mat2[3][1],
-                 mat1[2][0]*mat2[0][2]+mat1[2][1]*mat2[1][2]+mat1[2][2]*mat2[2][2]+mat1[2][3]*mat2[3][2],
-                 mat1[2][0]*mat2[0][3]+mat1[2][1]*mat2[1][3]+mat1[2][2]*mat2[2][3]+mat1[2][3]*mat2[3][3]],
-                
-                [mat1[3][0]*mat2[0][0]+mat1[3][1]*mat2[1][0]+mat1[3][2]*mat2[2][0]+mat1[3][3]*mat2[3][0],
-                 mat1[3][0]*mat2[0][1]+mat1[3][1]*mat2[1][1]+mat1[3][2]*mat2[2][1]+mat1[3][3]*mat2[3][1],
-                 mat1[3][0]*mat2[0][2]+mat1[3][1]*mat2[1][2]+mat1[3][2]*mat2[2][2]+mat1[3][3]*mat2[3][2],
-                 mat1[3][0]*mat2[0][3]+mat1[3][1]*mat2[1][3]+mat1[3][2]*mat2[2][3]+mat1[3][3]*mat2[3][3]]];
+    global_scale = FloatProperty(
+            name="Scale",
+            description="Scale all data",
+            min=0.01, max=1000.0,
+            soft_min=0.01,
+            soft_max=1000.0,
+            default=1.0,
+            );
 
-        
-    def getMatrix(self, bone):
-        if (bone.parent is None):
-            return bone.matrix_channel;
-        else:
-            return self.mul(self.getMatrix(bone.parent), bone.matrix_channel);
+    axis_forward = EnumProperty(
+            name="Forward",
+            items=(('X', "X Forward", ""),
+                   ('Y', "Y Forward", ""),
+                   ('Z', "Z Forward", ""),
+                   ('-X', "-X Forward", ""),
+                   ('-Y', "-Y Forward", ""),
+                   ('-Z', "-Z Forward", ""),
+                   ),
+            default='-Z',
+            );
 
-    def writeAnimation(self, f, scene, startFrame, endFrame, bone):
+    axis_up = EnumProperty(
+            name="Up",
+            items=(('X', "X Up", ""),
+                   ('Y', "Y Up", ""),
+                   ('Z', "Z Up", ""),
+                   ('-X', "-X Up", ""),
+                   ('-Y', "-Y Up", ""),
+                   ('-Z', "-Z Up", ""),
+                   ),
+            default='Y',
+            );
+
+    
+    def writeAnimation(self, f, scene, startFrame, endFrame, bone, r_matrix):
         indent = "        ";
         for frame in range(startFrame, endFrame+1):
             scene.frame_set(frame);
-            rot = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
-            m = self.mul(rot, self.getMatrix(bone));
+            m = r_matrix*bone.matrix_channel;
             f.write(indent + str(frame) + ": ");
-            f.write("%.6f, %.6f, %.6f, %.6f, " % (m[0][0], m[1][0], m[2][0], m[3][0]));
-            f.write("%.6f, %.6f, %.6f, %.6f, " % (m[0][1], m[1][1], m[2][1], m[3][1]));
-            f.write("%.6f, %.6f, %.6f, %.6f, " % (m[0][2], m[1][2], m[2][2], m[3][2]));
-            f.write("%.6f, %.6f, %.6f, %.6f" % (m[0][3], m[1][3], m[2][3], m[3][3]));
+            f.write("%.6f, %.6f, %.6f, %.6f, " % (m[0][0], m[0][1], m[0][2], m[0][3]));
+            f.write("%.6f, %.6f, %.6f, %.6f, " % (m[1][0], m[1][1], m[1][2], m[1][3]));
+            f.write("%.6f, %.6f, %.6f, %.6f, " % (m[2][0], m[2][1], m[2][2], m[2][3]));
+            f.write("%.6f, %.6f, %.6f, %.6f  " % (m[3][0], m[3][1], m[3][2], m[3][3]));
             f.write("\n");
 
-    def saveSAM(self, context, path, startFrame, endFrame):
+    def saveSAM(self, context, path, startFrame, endFrame, rot_matrix):
         f = open(path, "w", encoding="utf8", newline="\n");
 
         scene = context.scene;
         obj = context.object;
         armature = obj.parent;
 
-        f.write("Skeleton: skeleton://please edit path to skeleton.ske\n");
+        f.write("Skeleton: skeleton://Spout/entities/Spouty/spouty.ske\n");
         f.write("frames: %d\n" % (endFrame - startFrame + 1));
         f.write("delay: %.6f\n" % (1.0 / (scene.render.fps / scene.render.fps_base)));
         f.write("bones_data:\n");
         
         for bone in armature.pose.bones:
             f.write("    " + bone.name + ":\n");
-            self.writeAnimation(f, scene, startFrame, endFrame, bone);
+            self.writeAnimation(f, scene, startFrame, endFrame, bone, rot_matrix);
         
         f.close();
         return {'FINISHED'};
@@ -90,7 +101,18 @@ class ExportSAM(bpy.types.Operator, ExportHelper):
         path = self.as_keywords()["filepath"];
         start = self.as_keywords()["startFrame_prop"];
         end = self.as_keywords()["endFrame_prop"];
-        return self.saveSAM(context, path, start, end);
+        global_matrix = Matrix()
+        
+        global_matrix[0][0] = \
+        global_matrix[1][1] = \
+        global_matrix[2][2] = self.global_scale
+
+        global_matrix = (global_matrix *
+                         axis_conversion(to_forward=self.axis_forward,
+                                         to_up=self.axis_up,
+                                         ).to_4x4())
+        
+        return self.saveSAM(context, path, start, end, global_matrix);
 
 def menu_func(self, context):
     self.layout.operator(ExportSAM.bl_idname, text="Spout Animation Model (.sam)");
