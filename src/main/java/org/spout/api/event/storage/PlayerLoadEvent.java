0dd9a9a0d0e9d99e386cce6ad47b2d1e64ac5b6a
Afforess <afforess@gmail.com>
2013-02-24 23:05:44 -0500

    Allow plugins to take over player saving/loading

--- src/main/java/org/spout/api/event/storage/PlayerLoadEvent.java
+++ src/main/java/org/spout/api/event/storage/PlayerLoadEvent.java
@@ -24,48 +24,96 @@
  * License and see <http://spout.in/licensev1> for the full license, including
  * the MIT license.
  */
+/*
+* This file is part of SpoutAPI.
+ *
+ * Copyright (c) 2011-2012, SpoutDev <http://www.spout.org/>
+ * SpoutAPI is licensed under the SpoutDev License Version 1.
+ *
+ * SpoutAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * In addition, 180 days after any changes are published, you can use the
+ * software, incorporating those changes, under the terms of the MIT license,
+ * as described in the SpoutDev License Version 1.
+ *
+ * SpoutAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License,
+ * the MIT license and the SpoutDev License Version 1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
+ * License and see <http://www.spout.org/SpoutDevLicenseV1.txt> for the full license,
+ * including the MIT license.
+ */
 package org.spout.api.event.storage;
 
-import org.spout.api.entity.Player;
+import org.spout.api.entity.PlayerSnapshot;
 import org.spout.api.event.Event;
 import org.spout.api.event.HandlerList;
 
 /**
- * Called when data about a player needs to be loaded, usually right after a player session begins.
+ * Called before an player data is loaded from disk.
+ * <p>
+ * PlayerLoadEvents are async events and will never be executed on the
+ * main thread. Be sure to synchronize your data structures.
+ * </p>
  */
 public class PlayerLoadEvent extends Event {
 	private static HandlerList handlers = new HandlerList();
-	private boolean loaded = false;
-	private Player player;
+	private final String name;
+	private PlayerSnapshot snapshot = null;
 
-	public PlayerLoadEvent(Player player) {
-		this.player = player;
+	public PlayerLoadEvent(String name) {
+		this.name = name;
 	}
 
 	/**
-	 * Gets the player whose data is being loaded.
-	 * @return player
+	 * Gets the name of the player to load the data for
+	 * 
+	 * @return player name
 	 */
-	public Player getPlayer() {
-		return player;
+	public String getName() {
+		return name;
 	}
 
 	/**
-	 * True if a plugin has already loaded this data.
-	 * @return loaded
+	 * Gets the player snapshot to be loaded. It may be null.
+	 * <p>
+	 * If the player snapshot is null after the event has been fired, the
+	 * engine will use the default NBT player loading process. If the snapshot
+	 * is not-null, it will use the snapshot to restore the player state.
+	 * <br/><br/>
+	 * Plugins that wish to override the default saving and loading process
+	 * should set the snapshot.
+	 * </p>
+	 * 
+	 * @return snapshot
 	 */
-	public boolean isLoaded() {
-		return loaded;
+	public PlayerSnapshot getSnapshot() {
+		return snapshot;
 	}
 
 	/**
-	 * Sets the loaded state of this event.<br/>
-	 * <p/>
-	 * If the data is not reported loaded after it has been called, it will be loaded by the default save handler.
-	 * @param save
+	 * Sets the player snapshot to be loaded. Setting the snapshot to null causes
+	 * the engine to use the default NBT loading process.
+	 * <p>
+	 * If the player snapshot is null after the event has been fired, the
+	 * engine will use the default NBT player loading process. If the snapshot
+	 * is not-null, it will use the snapshot to restore the player state.
+	 * <br/><br/>
+	 * Plugins that wish to override the default saving and loading process
+	 * should set the snapshot.
+	 * </p>
+	 * 
+	 * @return snapshot
 	 */
-	public void setLoaded(boolean save) {
-		loaded = save;
+	public void setSnapshot(PlayerSnapshot snapshot) {
+		this.snapshot = snapshot;
 	}
 
 	@Override
