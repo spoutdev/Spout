a377e48e66d41b6f23bbab761ec95641fd0253eb
Chris Sanders <zidane@outlook.com>
2013-03-05 22:37:56 -0600

    Headers. Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/test/java/org/spout/math/BasicMathTest.java
+++ src/test/java/org/spout/math/BasicMathTest.java
@@ -24,32 +24,6 @@
  * License and see <http://spout.in/licensev1> for the full license, including
  * the MIT license.
  */
-/*
- * This file is part of SpoutAPI.
- *
- * Copyright (c) 2011-2012, Spout LLC <http://www.spout.org/>
- * SpoutAPI is licensed under the Spout License Version 1.
- *
- * SpoutAPI is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * In addition, 180 days after any changes are published, you can use the
- * software, incorporating those changes, under the terms of the MIT license,
- * as described in the Spout License Version 1.
- *
- * SpoutAPI is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
- * more details.
- *
- * You should have received a copy of the GNU Lesser General Public License,
- * the MIT license and the Spout License Version 1 along with this program.
- * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
- * License and see <http://spout.in/licensev1> for the full license, including
- * the MIT license.
- */
 package org.spout.math;
 
 import org.junit.Test;
@@ -62,8 +36,6 @@ import static org.spout.math.GenericMath.roundUpPow2;
 import static org.spout.math.TestUtils.eps;
 
 public final class BasicMathTest {
-	private static final double fastError = 0.0015;
-
 	@Test
 	public void testMean() {
 		int[] intTestValues = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
