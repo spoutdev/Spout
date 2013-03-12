182e8c8c7c9f1ad332cc099d8718caddd81e0902
Wulfspider <hello@wulfspider.com>
2013-03-11 22:52:11 -0400

    Cleanup and add headers.

--- src/main/java/org/spout/logging/jline/JLineHandler.java
+++ src/main/java/org/spout/logging/jline/JLineHandler.java
@@ -1,3 +1,29 @@
+/*
+ * This file is part of Spout.
+ *
+ * Copyright (c) 2013 Spout LLC <http://www.spout.org/>
+ * Spout is licensed under the Spout License Version 1.
+ *
+ * Spout is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * In addition, 180 days after any changes are published, you can use the
+ * software, incorporating those changes, under the terms of the MIT license,
+ * as described in the Spout License Version 1.
+ *
+ * Spout is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License,
+ * the MIT license and the Spout License Version 1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
+ * License and see <http://spout.in/licensev1> for the full license, including
+ * the MIT license.
+ */
 package org.spout.logging.jline;
 
 import java.io.IOException;
@@ -50,7 +76,7 @@ public class JLineHandler extends Handler {
 		Completor[] list = completers.toArray(new Completor[completers.size() + 1]);
 		list[list.length - 1] = new NullCompletor();
 		reader.addCompletor(new ArgumentCompletor(list));
-		
+
 		ConsoleCommandThread commandThread = new ConsoleCommandThread();
 		commandThread.start();
 	}
@@ -152,7 +178,7 @@ public class JLineHandler extends Handler {
 		try {
 			reader.killLine();
 			reader.flushConsole();
-		} catch (IOException e) {}
+		} catch (IOException e) {
+		}
 	}
-
 }
