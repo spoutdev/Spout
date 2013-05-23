005a82aac6e0d3a56deaaf6ad1c4259a895a0938
Chris Sanders <zidane@outlook.com>
2013-05-22 22:46:22 -0500

    Cleaned up Client interface.
    -- Unexposed worldChanged (this should not be allowed for plugins).
    -- Removed activeCamera (use entity.getType(Camera.class) or an extension
       of the camera interface you may have).
    -- Remove redundant active naming. This will make more sense when the
       Engine interface no longer has the getPlayer/getWorld methods (they are
       going into Server).
    -- Added a convienent getWorld which is always the world the client player
       is in as well as the currently rendererd world.
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spout/api/Client.java
+++ src/main/java/org/spout/api/Client.java
@@ -26,17 +26,14 @@
  */
 package org.spout.api;
 
-import java.util.UUID;
-
 import org.spout.api.audio.SoundManager;
+import org.spout.api.entity.Player;
 import org.spout.api.geo.World;
 import org.spout.api.gui.ScreenStack;
 import org.spout.api.input.InputManager;
 import org.spout.api.math.Vector2;
-import org.spout.api.entity.Player;
 import org.spout.api.plugin.PluginStore;
 import org.spout.api.protocol.PortBinding;
-import org.spout.api.render.Camera;
 import org.spout.api.render.RenderMode;
 
 /**
@@ -44,49 +41,39 @@ import org.spout.api.render.RenderMode;
  */
 public interface Client extends Engine {
 	/**
-	 * Gets the active player, connected to the local machine.
-	 *
-	 * @return active player
+	 * Gets the player on the local machine (the one who is using the client).
+	 * @return player
 	 */
-	public Player getActivePlayer();
+	public Player getPlayer();
 
 	/**
-	 * The Camera object is the viewport into the scene
+	 * Gets the current world in-which the player on the local machine is within.
 	 *
-	 * @return the location and view of the camera
+	 * This is always the world the client is currently rendering.
+	 * @return world
 	 */
-	public Camera getActiveCamera();
-
-	/**
-	 * Sets the camera to the active camera.
-	 * There can only be one camera active at a time
-	 */
-	public void setActiveCamera(Camera activeCamera);
+	public World getWorld();
 
 	/**
 	 * Gets client specific information for plugins.
-	 *
 	 * @return plugin store
 	 */
 	public PluginStore getPluginStore();
 
 	/**
 	 * Gets the current {@link RenderMode} that the client is running in.
-	 *
 	 * @return render mode
 	 */
 	public RenderMode getRenderMode();
 
 	/**
 	 * Gets the sound manager for the client. Used to create sound sources.
-	 * 
 	 * @return The client's sound manager.
 	 */
 	public SoundManager getSoundManager();
 
 	/**
 	 * Gets the input manager for the client. Keybindings are registered here.
-	 *
 	 * @return The client's input manager
 	 */
 	public InputManager getInputManager();
@@ -94,31 +81,19 @@ public interface Client extends Engine {
 	/**
 	 * Returns the current IP address the client is connected to.
 	 * If the client is not connected to a server, this returns null.
-	 *
 	 * @return address
 	 */
 	public PortBinding getAddress();
 
 	/**
-	 * This method is called to notify the client that its world needs to change
-	 *
-	 * @param name The name of the new world
-	 * @param uuid The world's uuid
-	 * @param datatable The world's datatable data
-	 * @return The new world
-	 */
-	public World worldChanged(String name, UUID uuid, byte[] datatable);
-
-	/**
 	 * Returns the resolution of the window, in pixels.
-	 * 
 	 * @return the resolution of the window.
 	 */
 	public Vector2 getResolution();
 
 	/**
 	 * Returns the aspect ratio of the client, in pixels.
-	 *
+	 * <p/>
 	 * Ratio = (screen width / screen height)
 	 * @return The ratio as a float
 	 */
@@ -126,7 +101,6 @@ public interface Client extends Engine {
 
 	/**
 	 * Returns the screen stack.
-	 * 
 	 * @return the screen stack.
 	 */
 	public ScreenStack getScreenStack();
