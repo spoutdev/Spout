86f93cc17d95ed4a9aaee34fb17b1a861a8fe656
Jack Huey <kitskub@gmail.com>
2013-06-05 17:26:49 -0500

    Make connecting to a server the first thing the client does
    Signed-off-by: Jack Huey kitskub@gmail.com

--- src/main/java/org/spout/engine/SpoutEngine.java
+++ src/main/java/org/spout/engine/SpoutEngine.java
@@ -28,6 +28,7 @@ package org.spout.engine;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.lang.reflect.Constructor;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetSocketAddress;
@@ -48,7 +49,6 @@ import java.util.logging.Logger;
 
 import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.jboss.netty.channel.group.ChannelGroup;
-import org.jboss.netty.channel.group.ChannelGroupFuture;
 import org.jboss.netty.channel.group.DefaultChannelGroup;
 
 import org.spout.api.Engine;
@@ -116,10 +116,9 @@ import org.spout.engine.util.thread.snapshotable.SnapshotableReference;
 import org.spout.engine.world.MemoryReclamationThread;
 import org.spout.engine.world.SpoutRegion;
 import org.spout.engine.world.SpoutWorld;
-import org.spout.engine.world.WorldSavingThread;
-
 import static org.spout.api.lang.Translation.log;
 import static org.spout.api.lang.Translation.tr;
+import org.spout.api.plugin.PluginLoader;
 
 public abstract class SpoutEngine implements AsyncManager, Engine {
 	private static final Logger logger = Logger.getLogger("Spout");
@@ -178,6 +177,13 @@ public abstract class SpoutEngine implements AsyncManager, Engine {
 			new TicklockMonitor().start();
 			new DeadlockMonitor().start();
 		}
+        
+		// Must register protocol on init or client session can't happen
+		Protocol.registerProtocol(new SpoutProtocol());
+		if (Protocol.getProtocol("Spout") == null) {
+			throw new IllegalStateException("SpoutProtocol was not successfully registered!");
+		}
+		loadPluginsAndProtocol();
 	}
 
 	@Override
@@ -225,8 +231,6 @@ public abstract class SpoutEngine implements AsyncManager, Engine {
 			AnnotatedCommandExecutorFactory.create(new AnnotatedCommandExecutorTest.ChildExecutor(), cmdManager.getCommand("root"));
 		}
 
-		Protocol.registerProtocol(new SpoutProtocol());
-
 		//Setup the Material Registry
 		engineItemMap = MaterialRegistry.setupRegistry();
 		//Setup the Biome Registry
@@ -235,8 +239,7 @@ public abstract class SpoutEngine implements AsyncManager, Engine {
 		engineLightingMap = LightingRegistry.setupRegistry();
 
 		// Start loading plugins
-		loadPlugins();
-		postPluginLoad(config);
+		setupBindings(config);
 		enablePlugins();
 
 		if (checkWorlds) {
@@ -265,12 +268,12 @@ public abstract class SpoutEngine implements AsyncManager, Engine {
 	}
 
 	/**
-	 * This method is called after {@link #loadPlugins()} but before {@link #enablePlugins()}
+	 * This method is called before {@link #enablePlugins()}
 	 */
-	protected void postPluginLoad(SpoutConfiguration config) {
+	protected void setupBindings(SpoutConfiguration config) {
 	}
 
-	public void loadPlugins() {
+	private void loadPluginsAndProtocol() {
 		pluginManager.clearPlugins();
 		pluginManager.installUpdates();
 
