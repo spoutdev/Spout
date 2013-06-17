d86a821184b86b9b7526efab3dd1390183f16cd9
Afforess <afforess@gmail.com>
2013-06-16 22:53:23 -0400

    Fix exception logging

--- src/main/java/org/spout/logging/MessageFormatter.java
+++ src/main/java/org/spout/logging/MessageFormatter.java
@@ -1,5 +1,7 @@
 package org.spout.logging;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
@@ -11,6 +13,11 @@ public class MessageFormatter extends Formatter{
 			for (int i = 0; i < record.getParameters().length; i++) {
 				message = message.replaceAll(new StringBuilder("\\{").append(i).append("}").toString(), (record.getParameters()[i] == null ? "null" : record.getParameters()[i].toString()));
 			}
+		}
+		if (record.getThrown() != null) {
+			StringWriter sink = new StringWriter();
+			record.getThrown().printStackTrace(new PrintWriter(sink, true));
+			message += sink.toString();
 		}
 		return message;
 	}
