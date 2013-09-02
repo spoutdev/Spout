e228e93fc6e480a8ec09c7735f583dca15bfbefc
Aleksi Sapon <alek42008@yahoo.com>
2013-09-02 16:08:26 -0400

    Client render window opens and closes correctly. Still blank.
    I still need to figure out where to do the meshing. Does anyone want to
    help?
    Signed-off-by: Aleksi Sapon <QcTechs@gmail.com>

---
+++ engine/src/main/java/org/spout/engine/filesystem/resource/loader/ShaderProgramLoader.java
@@ -0,0 +1,97 @@
+/*
+ * This file is part of Spout.
+ *
+ * Copyright (c) 2011 Spout LLC <http://www.spout.org/>
+ * Spout is licensed under the Spout License Version 1.
+ *
+ * Spout is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * In addition, 180 days after any changes are published, you can use the
+ * software, incorporating those changes, under the terms of the MIT license,
+ * as described in the Spout License Version 1.
+ *
+ * Spout is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License,
+ * the MIT license and the Spout License Version 1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
+ * License and see <http://spout.in/licensev1> for the full license, including
+ * the MIT license.
+ */
+package org.spout.engine.filesystem.resource.loader;
+
+import java.io.InputStream;
+
+import org.spout.api.Engine;
+import org.spout.api.Spout;
+import org.spout.api.render.RenderMode;
+import org.spout.api.resource.ResourceLoader;
+
+import org.spout.cereal.config.ConfigurationException;
+import org.spout.cereal.config.ConfigurationNode;
+import org.spout.cereal.config.yaml.YamlConfiguration;
+import org.spout.engine.SpoutClient;
+import org.spout.renderer.gl.Program;
+import org.spout.renderer.gl.Shader;
+import org.spout.renderer.gl.Shader.ShaderType;
+
+/**
+ *
+ */
+public class ShaderProgramLoader extends ResourceLoader {
+	public ShaderProgramLoader() {
+		super("shader", "shader://Spout/fallbacks/fallback.ssf");
+	}
+
+	@Override
+	public Program load(InputStream in) {
+		final YamlConfiguration configuration = new YamlConfiguration(in);
+		try {
+			configuration.load();
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		}
+
+		final Engine engine = Spout.getEngine();
+		if (!(engine instanceof SpoutClient)) {
+			throw new IllegalStateException("Shaders can only be loaded on the client.");
+		}
+		final SpoutClient client = (SpoutClient) engine;
+		final RenderMode mode = client.getRenderMode();
+
+		final ConfigurationNode shaderNode = configuration.getNode(mode.toString());
+		if (shaderNode == null || !shaderNode.hasChildren()) {
+			throw new IllegalStateException("Missing version in spout shader file: " + mode);
+		}
+
+		final Shader vertex = client.getRenderer().getGL().createShader();
+		String shaderFile = shaderNode.getNode("Vertex").getString();
+		InputStream shaderSource = Spout.getFileSystem().getResourceStream(shaderFile);
+		if (shaderSource == null) {
+			throw new IllegalStateException("Shader file not found: " + shaderFile);
+		}
+		vertex.setSource(shaderSource);
+		vertex.setType(ShaderType.VERTEX);
+
+		final Shader fragment = client.getRenderer().getGL().createShader();
+		shaderFile = shaderNode.getNode("Fragment").getString();
+		shaderSource = Spout.getFileSystem().getResourceStream(shaderFile);
+		if (shaderSource == null) {
+			throw new IllegalStateException("Shader file not found: " + shaderFile);
+		}
+		fragment.setSource(shaderSource);
+		fragment.setType(ShaderType.FRAGMENT);
+
+		final Program program = client.getRenderer().getGL().createProgram();
+		program.addShader(vertex);
+		program.addShader(fragment);
+
+		return program;
+	}
+}
