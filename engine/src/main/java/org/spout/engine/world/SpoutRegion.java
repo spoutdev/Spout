edc936b929a7caadd9b0102e9430d41c5849827f
Jack Huey <kitskub@gmail.com>
2013-09-03 17:23:02 -0500

    Rename ChunkSnapshotModel to ChunkSnapshotGroup
    Signed-off-by: Jack Huey kitskub@gmail.com

--- engine/src/main/java/org/spout/engine/world/SpoutRegion.java
+++ engine/src/main/java/org/spout/engine/world/SpoutRegion.java
@@ -67,6 +67,7 @@ import org.spout.api.lighting.LightingManager;
 import org.spout.api.material.BlockMaterial;
 import org.spout.api.material.DynamicUpdateEntry;
 import org.spout.api.material.block.BlockFace;
+import org.spout.api.material.block.BlockFaces;
 import org.spout.api.material.range.EffectRange;
 import org.spout.api.math.IntVector3;
 import org.spout.api.math.ReactConverter;
@@ -773,24 +774,23 @@ public class SpoutRegion extends Region implements AsyncManager {
 				processChunkUpdatedEvent(spoutChunk);
 				if (cached != null) {
 					SpoutChunkSnapshot[][][] snapshots = new SpoutChunkSnapshot[3][3][3];
-					for (int x = -1; x <= 1; x++) {
-						for (int y = -1; y <= 1; y++) {
-							for (int z = -1; z <= 1; z++) {
-								final int localX = x + spoutChunk.getX();
-								final int localY = x + spoutChunk.getY();
-								final int localZ = x + spoutChunk.getZ();
-								final MultiKey key = new MultiKey(localX, localY, localZ);
-								SpoutChunkSnapshot get = cached.get(key);
-								if (get == null) {
-									SpoutChunkSnapshot snapshot = getWorld().getChunk(localX, localY, localZ).getSnapshot(ChunkSnapshot.SnapshotType.BOTH, ChunkSnapshot.EntityType.NO_ENTITIES, ChunkSnapshot.ExtraData.NO_EXTRA_DATA);
-									cached.put(key, snapshot);
-									get = snapshot;
-								}
-								snapshots[x + 1][y + 1][z + 1] = get;
-							}
+					for (BlockFace face : BlockFaces.BTNSWE) {
+						final int x = face.getOffset().getFloorX();
+						final int y = face.getOffset().getFloorY();
+						final int z = face.getOffset().getFloorZ();
+						final int localX = x + spoutChunk.getX();
+						final int localY = y + spoutChunk.getY();
+						final int localZ = z + spoutChunk.getZ();
+						final MultiKey key = new MultiKey(localX, localY, localZ);
+						SpoutChunkSnapshot get = cached.get(key);
+						if (get == null) {
+							SpoutChunkSnapshot snapshot = getWorld().getChunk(localX, localY, localZ).getSnapshot(ChunkSnapshot.SnapshotType.BOTH, ChunkSnapshot.EntityType.NO_ENTITIES, ChunkSnapshot.ExtraData.NO_EXTRA_DATA);
+							cached.put(key, snapshot);
+							get = snapshot;
 						}
+						snapshots[x + 1][y + 1][z + 1] = get;
 					}
-					SpoutChunkSnapshotModel model = new SpoutChunkSnapshotModel(getWorld(), spoutChunk.getX(), spoutChunk.getY(), spoutChunk.getZ(), snapshots, 1, false, System.currentTimeMillis());
+					SpoutChunkSnapshotGroup group = new SpoutChunkSnapshotGroup(getWorld(), spoutChunk.getX(), spoutChunk.getY(), spoutChunk.getZ(), snapshots, 1, false, System.currentTimeMillis());
 					// TODO: submit this ChunkSnapshotModel to mesh thread for meshing
 				}
 			}
