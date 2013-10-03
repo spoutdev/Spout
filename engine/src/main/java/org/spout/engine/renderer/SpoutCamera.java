79bf599e03ceaf575347f35510733ad681636d2d
Waterpicker <Waterpicker@rocketmail.com>
2013-10-03 00:24:41 -0500

    Update SpoutCamera.java
    Fixed formatting

--- engine/src/main/java/org/spout/engine/renderer/SpoutCamera.java
+++ engine/src/main/java/org/spout/engine/renderer/SpoutCamera.java
@@ -24,10 +24,7 @@
  * License and see <http://spout.in/licensev1> for the full license, including
  * the MIT license.
  */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.spout.engine.renderer;
 
 import org.spout.api.render.Camera;
